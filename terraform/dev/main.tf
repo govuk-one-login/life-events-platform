89116bd41a2b8545a1678eab8479edbfd7952a58
Oskar Williams <oskar.williams@softwire.com>
2022-12-08 14:44:38 +0000

    feat: Add github actions to build and push to ECR image

---
+++ terraform/dev/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "gdx-data-share-poc-tfstate"
+    key            = "terraform-dev.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "gdx-data-share-poc-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      source      = "terraform"
+      repository  = "https://github.com/alphagov/gdx-data-share-poc"
+      environment = "dev"
+    }
+  }
+}

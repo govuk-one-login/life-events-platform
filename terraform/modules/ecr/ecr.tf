d9727786c1e33fd422b3c2603b2a88244f5f8cad
Oskar Williams <oskar.williams@softwire.com>
2022-12-09 12:22:44 +0000

    chore: Add more security features

--- terraform/modules/ecr/ecr.tf
+++ terraform/modules/ecr/ecr.tf
@@ -1,5 +1,18 @@
 resource "aws_ecr_repository" "ecr_repo" {
-  name = "ecr_repo"
+  name = "ecr-repo"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "KMS"
+    kms_key         = aws_kms_key.ecr_repo_key.arn
+  }
+}
+
+resource "aws_kms_key" "ecr_repo_key" {
+  description         = "ECR repository encryption key"
+  enable_key_rotation = true
 }
 
 resource "aws_ecr_registry_scanning_configuration" "ecr_scanning_configuration" {
@@ -8,7 +21,7 @@ resource "aws_ecr_registry_scanning_configuration" "ecr_scanning_configuration" 
   rule {
     scan_frequency = "SCAN_ON_PUSH"
     repository_filter {
-      filter      = "ecr_repo"
+      filter      = "ecr-repo"
       filter_type = "WILDCARD"
     }
   }

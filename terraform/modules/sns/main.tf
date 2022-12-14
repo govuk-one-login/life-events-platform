6eccf8811dc7c8ecbaa4aca2b78e5a719d118b81
Olly Levett <oliver.levett@softwire.com>
2022-12-14 12:37:06 +0000

    feat: Encrypt SNS queue

--- terraform/modules/sns/main.tf
+++ terraform/modules/sns/main.tf
@@ -1,8 +1,9 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_sns_topic" "topic" {
-  name_prefix  = "${var.environment}-gdx-sns-topic"
-  display_name = var.topic_display_name
+  name_prefix       = "${var.environment}-gdx-sns-topic"
+  display_name      = var.topic_display_name
+  kms_master_key_id = aws_kms_key.kms_key.arn
 }
 
 resource "aws_iam_user" "sns_user" {
@@ -31,4 +32,9 @@ resource "aws_iam_user_policy" "policy" {
   name   = "sns-topic"
   policy = data.aws_iam_policy_document.policy.json
   user   = aws_iam_user.user.name
+}
+
+resource "aws_kms_key" "kms_key" {
+  enable_key_rotation = true
+  description         = "Key used to encrypt sns queue"
 }

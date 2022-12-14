9fe5ec5089fecd5e0e55970c54a02be8be613f78
Olly Levett <oliver.levett@softwire.com>
2022-12-14 14:05:58 +0000

    chore: Reformat sqs

--- terraform/modules/data-share-service/sqs.tf
+++ terraform/modules/data-share-service/sqs.tf
@@ -1,23 +1,23 @@
 module "data_processor_queue" {
   source      = "../sqs"
-  queue_name        = "${var.environment}-gdx-data-share-poc-data-processor"
+  queue_name  = "${var.environment}-gdx-data-share-poc-data-processor"
   environment = var.environment
 }
 
 module "audit_queue" {
   source      = "../sqs"
-  queue_name        = "${var.environment}-gdx-data-share-poc-audit-queue"
+  queue_name  = "${var.environment}-gdx-data-share-poc-audit-queue"
   environment = var.environment
 }
 
 module "outbound_adaptor_queue" {
   source      = "../sqs"
-  queue_name        = "${var.environment}-gdx-data-share-poc-outbound-queue"
+  queue_name  = "${var.environment}-gdx-data-share-poc-outbound-queue"
   environment = var.environment
 }
 
 module "other_department_queue" {
   source      = "../sqs"
-  queue_name        = "${var.environment}-gdx-data-share-poc-other-department-queue"
+  queue_name  = "${var.environment}-gdx-data-share-poc-other-department-queue"
   environment = var.environment
 }

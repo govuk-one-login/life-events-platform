8b4ae6a4298f4efdda1d765a29655f646b926f0e
Oskar Williams <oskar.williams@softwire.com>
2022-12-15 13:47:28 +0000

    feat: Use scope identifiers

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -6,16 +6,14 @@ resource "aws_cognito_user_pool_client" "legacy_inbound_adapter" {
   name                 = "${var.environment}-legacy-inbound-adapter"
   user_pool_id         = aws_cognito_user_pool.pool.id
   allowed_oauth_flows  = ["client_credentials"]
-  allowed_oauth_scopes = ["data_receiver/notify"]
-
+  allowed_oauth_scopes = aws_cognito_resource_server.data_receiver.scope_identifiers
 }
 
 resource "aws_cognito_user_pool_client" "legacy_outbound_adapter" {
   name                 = "${var.environment}-legacy-outbound-adapter"
   user_pool_id         = aws_cognito_user_pool.pool.id
   allowed_oauth_flows  = ["client_credentials"]
-  allowed_oauth_scopes = ["data_retriever/read"]
-
+  allowed_oauth_scopes = aws_cognito_resource_server.data_retriever.scope_identifiers
 }
 
 resource "aws_cognito_resource_server" "data_receiver" {

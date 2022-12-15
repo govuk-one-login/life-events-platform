adb81f57447b96c608aae257a76d6b6c2d383818
Olly Levett <oliver.levett@softwire.com>
2022-12-15 09:36:58 +0000

    feat: Add first pass at RDS configuration for POC (#55)

---
+++ terraform/modules/data-share-service/rds.tf
@@ -0,0 +1,25 @@
+resource "random_string" "rds_username" {
+  length  = 16
+  special = false
+}
+
+resource "random_password" "rds_password" {
+  length  = 16
+  special = false
+}
+
+resource "aws_rds_cluster" "rds_postgres_cluster" {
+  cluster_identifier = "${var.environment}-rds-db"
+  engine             = "aurora-postgresql"
+  availability_zones = ["eu-west-2a"]
+  database_name      = "${var.environment}-rds-db"
+  master_username    = random_string.rds_username.result
+  master_password    = random_password.rds_password.result
+
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+
+  kms_key_id        = aws_kms_key.rds_key.arn
+  storage_encrypted = true
+}
+

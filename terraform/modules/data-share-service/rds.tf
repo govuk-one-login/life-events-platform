f9f2990eb0d913129c4ac3b5c7c651df3c618074
Olly Levett <oliver.levett@softwire.com>
2022-12-15 10:36:10 +0000

    chore: Add additional availability zones to prevent replacement

--- terraform/modules/data-share-service/rds.tf
+++ terraform/modules/data-share-service/rds.tf
@@ -11,7 +11,7 @@ resource "random_password" "rds_password" {
 resource "aws_rds_cluster" "rds_postgres_cluster" {
   cluster_identifier = "${var.environment}-rds-db"
   engine             = "aurora-postgresql"
-  availability_zones = ["eu-west-2a"]
+  availability_zones = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   database_name      = "${var.environment}rdsdb"
   master_username    = random_string.rds_username.result
   master_password    = random_password.rds_password.result

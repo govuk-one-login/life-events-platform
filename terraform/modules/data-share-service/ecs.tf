e9eb4c6c7c68d10c74ca080c83343fefbf3ab1f7
CarGil <carly.gilson@softwire.com>
2022-12-19 14:55:07 +0000

    chore: Remove hmrc url mock value

--- terraform/modules/data-share-service/ecs.tf
+++ terraform/modules/data-share-service/ecs.tf
@@ -31,7 +31,6 @@ resource "aws_ecs_task_definition" "gdx_data_share_poc" {
         { "name" : "API_BASE_URL_LEV", "value" : "https://${var.lev_url}" },
         { "name" : "API_BASE_URL_ISSUER_URI", "value" : "https://${module.cognito.issuer_domain}" },
         { "name" : "API_BASE_URL_OAUTH", "value" : "https://${module.cognito.auth_domain}" },
-        { "name" : "API_BASE_URL_HMRC", "value" : "https://a0519c3b-e75b-41aa-b79d-7bb41871ec62.mock.pstmn.io" },
         { "name" : "API_BASE_URL_DATA_RECEIVER", "value" : "http://gdx-data-share-poc:8080" },
         { "name" : "API_BASE_URL_EVENT_DATA_RETRIEVAL", "value" : "http://localhost:8080" },
         { "name" : "API_BASE_S3_INGRESS", "value" : module.ingress.name },

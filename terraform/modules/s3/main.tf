8f06548030b37583cc1ff7d558086ad5a5963ab0
Oskar Williams <oskar.williams@softwire.com>
2022-12-19 15:13:21 +0000

    feat: Fix s3 naming

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "bucket" {
 #tfsec:ignore:aws-s3-enable-bucket-logging
 #tfsec:ignore:aws-s3-enable-versioning
 resource "aws_s3_bucket" "log_bucket" {
-  bucket = "${aws_s3_bucket.bucket.bucket}-logs-gdx-data-share-poc"
+  bucket = "${var.environment}-${var.name}-logs-gdx-data-share-poc"
 
   lifecycle {
     prevent_destroy = true

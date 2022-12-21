acdf84bf8f3ab7f5a7f8c56d75750b43131b3a61
CarGil <carly.gilson@softwire.com>
2022-12-21 15:50:29 +0000

    feat: Add metric for Event data receiver api calls

--- terraform/modules/data-share-service/cloudwatch.tf
+++ terraform/modules/data-share-service/cloudwatch.tf
@@ -11,3 +11,27 @@ resource "aws_cloudwatch_log_group" "lb_sg_update" {
 
   kms_key_id = aws_kms_key.log_key.arn
 }
+
+resource "aws_cloudwatch_dashboard" "metrics_dashboard" {
+  dashboard_name = "metrics-dashboard"
+  dashboard_body = <<EOF
+{
+  "widgets": [
+    {
+      "type": "metric",
+      "properties": {
+        "metrics": [
+          [
+            "gdxApp",
+            "API_CALLS.IngestedEvents.count"
+          ]
+        ],
+        "period": 300,
+        "region": ${var.region},
+        "title": "API calls"
+      }
+    }
+  ]
+}
+EOF
+}

427ab83f6193c21c225bcec9dc48dbd14bd57510
Oskar Williams <oskar.williams@softwire.com>
2022-12-21 14:30:24 +0000

    feat: Fix consumer subscriptions and dev seed data

--- src/main/resources/db/migration/V1_6__ingress_and_egress_events.sql
+++ src/main/resources/db/migration/V1_6__ingress_and_egress_events.sql
@@ -4,6 +4,8 @@ rename to ingress_event_data;
 alter table event_type
 rename to ingress_event_type;
 
+drop table consumer_subscription;
+
 
 create table egress_event_type
 (
@@ -36,4 +38,27 @@ create table egress_event_data
     CONSTRAINT fk_event_type
         FOREIGN KEY (type_id)
             REFERENCES egress_event_type (id)
+);
+
+create table consumer_subscription
+(
+    id                   UUID PRIMARY KEY         NOT NULL DEFAULT gen_random_uuid(),
+    consumer_id          UUID                     NOT NULL,
+    event_type_id        UUID                     NOT NULL,
+    poll_client_id       VARCHAR(50)              NULL,
+    callback_client_id   VARCHAR(50)              NULL,
+    last_poll_event_time TIMESTAMP WITH TIME ZONE NULL,
+    push_uri             VARCHAR(400)             NULL,
+    nino_required        BOOLEAN                  NOT NULL DEFAULT false,
+    when_created         TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+
+    CONSTRAINT fk_event_type
+        FOREIGN KEY (event_type_id)
+            REFERENCES egress_event_type (id),
+
+    CONSTRAINT fk_consumer
+        FOREIGN KEY (consumer_id)
+            REFERENCES event_consumer (id),
+
+    CONSTRAINT uk_event_client UNIQUE (event_type_id, callback_client_id)
 );

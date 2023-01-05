802ad46430fa32c65960085384eb28a1e95c63a4
Oskar Williams <oskar.williams@softwire.com>
2023-01-05 14:00:24 +0000

    test: update integration test base

--- src/test/kotlin/uk/gov/gdx/datashare/integration/api/EventsQueryTest.kt
+++ src/test/kotlin/uk/gov/gdx/datashare/integration/api/EventsQueryTest.kt
@@ -2,8 +2,9 @@ package uk.gov.gdx.datashare.integration.api
 
 import org.junit.jupiter.api.Test
 import uk.gov.gdx.datashare.integration.IntegrationTestBase
+import uk.gov.gdx.datashare.integration.SqsIntegrationTestBase
 
-class EventsQueryTest : IntegrationTestBase() {
+class EventsQueryTest : SqsIntegrationTestBase() {
   @Test
   fun `Get events successfully queries db`() {
     webTestClient.get()

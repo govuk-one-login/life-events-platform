3378491d9758d5e2102ea808479e65bc43d94abf
Ethan Mills <ethan.mills@softwire.com>
2023-01-09 11:21:05 +0000

    fix: Apply IAM credentials change immediately

--- terraform/modules/data-share-service/rds.tf
+++ terraform/modules/data-share-service/rds.tf
@@ -16,6 +16,7 @@ resource "aws_rds_cluster" "rds_postgres_cluster" {
   master_username                     = random_string.rds_username.result
   master_password                     = random_password.rds_password.result
   iam_database_authentication_enabled = true
+  apply_immediately                   = true # TODO ethmil: remove once applied
 
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"

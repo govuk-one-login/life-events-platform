affc7a67b17ffd723d60a6643df8fe5c74e5c03e
oskarwilliams378 <oskar.williams@softwire.com>
2023-01-10 16:57:12 +0000

    feat: Add posting events to lambda (#191)

---
+++ terraform/modules/data-share-service/outputs.tf
@@ -0,0 +1,12 @@
+output "gdx_url" {
+  value = "https://${aws_cloudfront_distribution.gdx_data_share_poc.domain_name}"
+}
+output "token_auth_url" {
+  value = "${module.cognito.auth_domain}/oauth2/token"
+}
+output "len_client_id" {
+  value = module.cognito.len_client_id
+}
+output "len_client_secret" {
+  value = module.cognito.len_client_secret
+}
--- terraform/modules/len/lambdas/len.py
+++ terraform/modules/len/lambdas/len.py
@@ -1,16 +1,68 @@
 import logging
 import os
-import boto3
+from random import randint
+from urllib import request, parse
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def lambda_handler(event, context):
+gdx_url = os.environ["gdx_url"]
+events_url = gdx_url + "/events"
+auth_url = os.environ["auth_url"]
 
-    logger.info(f'Hello World!')
-    logger.info(f'## ENVIRONMENT VARIABLES: {os.environ}')
-    logger.info(f'## EVENT: {event}')
+client_id = os.environ["len_client_id"]
+client_secret = os.environ["len_client_secret"]
+
+
+def lambda_handler(event, context):
+    logger.info(f"## EVENT: {event}")
+
+    auth_request_data = parse.urlencode({
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret
+    }).encode()
+    auth_request = request.Request(
+        url=auth_url,
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        method="POST",
+        data=auth_request_data
+    )
+
+    auth_token = json.loads(request.urlopen(auth_request).read())["access_token"]
+
+    if event["detail-type"] == "Scheduled Event":
+        run_scheduled_job(auth_token)
+    else:
+        run_manual_job(auth_token)
 
     return {
-        'statusCode': 200,
+        "statusCode": 200,
     }
+
+
+def run_scheduled_job(auth_token: str):
+    for i in range(randint(0, 8)):
+        post_event(auth_token)
+
+
+def run_manual_job(auth_token: str):
+    post_event(auth_token)
+
+
+def post_event(auth_token: str):
+    death_certificate = 999999900 + randint(1, 72)
+    event_request = request.Request(
+        events_url,
+        data={
+            "eventType": "DEATH_NOTIFICATION",
+            "id": death_certificate
+        },
+        headers={
+            "Authorization": "Bearer " + auth_token
+        }
+    )
+    logger.info(f"## Posting event {death_certificate}")
+    response = json.loads(request.urlopen(event_request).read())
+    logger.info(f"## Event for {death_certificate} complete, response: {response}")

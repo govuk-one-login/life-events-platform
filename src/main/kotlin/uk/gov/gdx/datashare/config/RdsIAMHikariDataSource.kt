afa69b4e1c475860ef03dfa524e405fb4f42c2b8
Oskar Williams <oskar.williams@softwire.com>
2023-01-10 11:14:55 +0000

    Revert "feat: Use dynamic IAM token to access RDS database"
    This reverts commit 3cc9cc19aa9752a7a4f68c055f275d1179b1780a.

--- src/main/kotlin/uk/gov/gdx/datashare/config/RdsIAMHikariDataSource.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.gdx.datashare.config
-
-import com.zaxxer.hikari.HikariDataSource
-import uk.gov.gdx.datashare.service.DbCredentialsService
-
-class RdsIAMHikariDataSource(private val dbCredentialsService: DbCredentialsService) : HikariDataSource() {
-  override fun getPassword() = dbCredentialsService.password()
-}

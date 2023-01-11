a20fb8dca84d25dd14840df87e7ca3a54ebdfdcc
Oskar Williams <oskar.williams@softwire.com>
2023-01-11 11:32:01 +0000

    feat: Add dwp client

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -96,12 +96,21 @@ module "legacy_outbound_adaptor" {
   depends_on = [aws_cognito_resource_server.events]
 }
 
-
 module "len_mock" {
   source       = "../simple_user_pool_client"
   environment  = var.environment
   scope        = "${local.identifier}/${local.scope_publish}"
   name         = "len-mock"
+  user_pool_id = aws_cognito_user_pool.pool.id
+
+  depends_on = [aws_cognito_resource_server.events]
+}
+
+module "dwp" {
+  source       = "../simple_user_pool_client"
+  environment  = var.environment
+  scope        = "${local.identifier}/${local.scope_consume}"
+  name         = "dwp"
   user_pool_id = aws_cognito_user_pool.pool.id
 
   depends_on = [aws_cognito_resource_server.events]

a55a1ff4a2bee7d4f238d8f5dca8266946d40fb9
Ethan Mills <ethan.mills@softwire.com>
2023-01-13 11:02:49 +0000

    Revert "feat: Add 2 min wait time on code deploy to let instances start up (#221)"

--- terraform/modules/data-share-service/codedeploy.tf
+++ terraform/modules/data-share-service/codedeploy.tf
@@ -16,12 +16,11 @@ resource "aws_codedeploy_deployment_group" "gdx_data_share_poc" {
 
   blue_green_deployment_config {
     deployment_ready_option {
-      action_on_timeout    = "CONTINUE_DEPLOYMENT"
-      wait_time_in_minutes = 2
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
     }
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 2
+      termination_wait_time_in_minutes = 0
     }
   }
 

b5ebbbfdb32ca4777e5c0ec693204d2b1b257b02
Mjwillis <Mjwillis@users.noreply.github.com>
2023-01-17 11:22:24 +0000

    housekeeping: Automatically reformat terraform files

--- terraform/modules/data-share-service/subscription.tf
+++ terraform/modules/data-share-service/subscription.tf
@@ -2,12 +2,12 @@ resource "aws_sns_topic_subscription" "outbound_subscription" {
   topic_arn     = module.sns.sns_topic_arn
   protocol      = "sqs"
   endpoint      = module.outbound_adaptor_queue.queue_arn
-  filter_policy = jsonencode(map("consumer",list("Internal Adaptor", "all")))
+  filter_policy = jsonencode(map("consumer", list("Internal Adaptor", "all")))
 }
 
 resource "aws_sns_topic_subscription" "other_department_subscription" {
   topic_arn     = module.sns.sns_topic_arn
   protocol      = "sqs"
   endpoint      = module.other_department_queue.queue_arn
-  filter_policy = jsonencode(map("consumer",list("Pub/Sub Consumer", "all")))
+  filter_policy = jsonencode(map("consumer", list("Pub/Sub Consumer", "all")))
 }

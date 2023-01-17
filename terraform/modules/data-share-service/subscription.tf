1b3c21ce0ae1df17d75aed929320626463bb6ed8
Michael Willis <mike.willis@digital.cabinet-office.gov.uk>
2023-01-17 15:26:56 +0000

    Update terraform/modules/data-share-service/subscription.tf
    Co-authored-by: Ethan Mills <40488007+ethanmills@users.noreply.github.com>

--- terraform/modules/data-share-service/subscription.tf
+++ terraform/modules/data-share-service/subscription.tf
@@ -9,5 +9,5 @@ resource "aws_sns_topic_subscription" "other_department_subscription" {
   topic_arn     = module.sns.sns_topic_arn
   protocol      = "sqs"
   endpoint      = module.other_department_queue.queue_arn
-  filter_policy = jsonencode(map("consumer", list("Pub/Sub Consumer", "all")))
+  filter_policy = jsonencode({ "consumer" : ["Pub/Sub Consumer", "all"] })
 }

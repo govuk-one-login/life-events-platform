d1876a1d9b240ee377d5ea9adc60507251adfd94
CarGil <carly.gilson@softwire.com>
2023-01-18 12:15:29 +0000

    feat: update len lambda to update lev db

--- terraform/modules/len/lambdas/len.py
+++ terraform/modules/len/lambdas/len.py
@@ -5,6 +5,7 @@ import os
 from datetime import datetime
 from random import randint
 from urllib import request
+import psycopg2
 
 from common import get_auth_token
 
@@ -18,32 +19,17 @@ auth_url = os.environ["auth_url"]
 client_id = os.environ["len_client_id"]
 client_secret = os.environ["len_client_secret"]
 
-# Found at https://github.com/UKHomeOffice/lev-api/blob/master/mock/death_registration_v1.json
-validDeathCertificates = [
-    123456789,
-    999999901,
-    999999902,
-    999999903,
-    999999910,
-    999999920,
-    999999930,
-    999999931,
-    999999932,
-    999999933,
-    999999934,
-    999999935,
-    999999940,
-    999999941,
-    999999942,
-    999999950,
-    999999960,
-    999999961,
-    999999962,
-    999999963,
-    999999970,
-    999999971,
-    999999972
-]
+lev_username = os.environ["lev_rds_db_username"]
+lev_password = os.environ["lev_rds_db_password"]
+lev_db_name = os.environ["lev_rds_db_name"]
+lev_host = os.environ["lev_rds_db_host"]
+
+forenames_options = ["Tester", None]
+surname_options = ["SMITH", None]
+date_of_birth_options = ["1912-02-29", None]
+date_of_death_options = ["2012-02-29", None]
+sex_options = ["Male", "Female", "Indeterminate", None]
+address_options = ["10 Test Street", None]
 
 
 def lambda_handler(event, context):
@@ -76,10 +62,10 @@ def run_manual_job(auth_token: str):
 
 
 def post_event(auth_token: str):
-    death_certificate = validDeathCertificates[randint(0, len(validDeathCertificates) - 1)]
+    death_registration_id = create_death_registration()
     event_request_data = json.dumps({
         "eventType": "DEATH_NOTIFICATION",
-        "id": death_certificate
+        "id": death_registration_id
     }).encode("utf-8")
     event_request = request.Request(
         events_url,
@@ -90,6 +76,83 @@ def post_event(auth_token: str):
             "Content-Length": len(event_request_data)
         }
     )
-    logger.info(f"## Posting death certificate {death_certificate}")
+    logger.info(f"## Posting death certificate {death_registration_id}")
     request.urlopen(event_request).read()
-    logger.info(f"## Successfully posted death certificate {death_certificate}")
+    logger.info(f"## Successfully posted death certificate {death_registration_id}")
+
+
+def create_death_registration():
+    connection = psycopg2.connect(user=lev_username, password=lev_password, database=lev_db_name, host=lev_host)
+    cursor = connection.cursor()
+    cursor.execute("select nextval('death_registration_ids')")
+    death_registration_id = cursor.fetchone()
+
+    cursor.execute("""
+        INSERT INTO death_registration_v1 (id, data, forenames, surname, date_of_birth, date_of_death)
+        VALUES(%(id)s, %(data)s, %(forenames)s, %(surname)s, %(date_of_birth)s, %(date_of_death)s)
+        """,
+                   generate_death_registration(death_registration_id))
+
+    return death_registration_id
+
+
+def generate_death_registration(death_registration_id: int):
+    forenames = forenames_options[randint(0, len(forenames_options) - 1)]
+    surname = surname_options[randint(0, len(surname_options) - 1)]
+    date_of_birth = date_of_birth_options[randint(0, len(date_of_birth_options) - 1)]
+    date_of_death = date_of_death_options[randint(0, len(date_of_death_options) - 1)]
+
+    data = {
+               "id": death_registration_id,
+               "date": "2014-10-10",
+               "entryNumber": death_registration_id,
+               "registrar": {
+                   "signature": "A. Registrar",
+                   "designation": "Registrar",
+                   "subdistrict": "Test Subdistrict",
+                   "district": "Test District",
+                   "administrativeArea": "Reading"
+               },
+               "informant": {
+                   "forenames": "Informy",
+                   "surname": "McInformface",
+                   "address": "666 Inform House, 6 Inform street, Informington, Informshire",
+                   "qualification": "Life-long nemesis, Present at death",
+                   "signature": "I. McInformface"
+               },
+               "deceased": {
+                   "forenames": forenames,
+                   "surname": surname,
+                   "dateOfBirth": date_of_birth,
+                   "dateOfDeath": date_of_death,
+                   "ageAtDeath": "100 years",
+                   "birthplace": "Test address",
+                   "deathplace": "Test address",
+                   "sex": sex_options[randint(0, len(sex_options) - 1)],
+                   "address": address_options[randint(0, len(address_options) - 1)],
+                   "occupation": "Unemployed",
+                   "retired": False,
+                   "maidenSurname": "TESTER",
+                   "aliases": [],
+                   "causeOfDeath": "Old age",
+                   "certifiedBy": "A. Doctor MD",
+               },
+               "partner": {},
+               "mother": {},
+               "father": {},
+               "coroner": {},
+               "status": {
+                   "blocked": False,
+                   "correction": "None",
+                   "marginalNote": "None",
+                   "onAuthorityOfRegistrarGeneral": False
+               },
+           },
+    return {
+        "id": death_registration_id,
+        "data": data,
+        "forenames": forenames,
+        "surnames": surname,
+        "date_of_birth": date_of_birth,
+        "date_of_death": date_of_death,
+    }

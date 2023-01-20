175450ef1d40f813df21d1d9567d65069ffdb7ea
Olly Levett <oliver.levett@softwire.com>
2023-01-20 16:09:59 +0000

    feat: Remove audit SQS (#279)

--- terraform/modules/data-share-service/sqs.tf
+++ terraform/modules/data-share-service/sqs.tf
@@ -3,9 +3,3 @@ module "data_processor_queue" {
   queue_name  = "${var.environment}-gdx-data-share-poc-data-processor-queue"
   environment = var.environment
 }
-
-module "audit_queue" {
-  source      = "../sqs"
-  queue_name  = "${var.environment}-gdx-data-share-poc-audit-queue"
-  environment = var.environment
-}

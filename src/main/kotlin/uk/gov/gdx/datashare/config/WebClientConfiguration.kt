bebe2b39e4dde90b1f28e2b762320b037df39c9c
Ethan Mills <40488007+ethanmills@users.noreply.github.com>
2023-01-27 11:31:14 +0000

    fix: set connection params for webclient
    Aims to prevent connection closed by client errors
    https://github.com/reactor/reactor-netty/issues/1774

--- src/main/kotlin/uk/gov/gdx/datashare/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/config/WebClientConfiguration.kt
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
+import reactor.netty.resources.ConnectionProvider
 import java.time.Duration
 
 @Configuration
@@ -21,7 +22,14 @@ class WebClientConfiguration(
 
   @Bean
   fun levApiWebClient(): WebClient {
-    val httpClient = HttpClient.create().responseTimeout(Duration.ofMinutes(2))
+    val connectionProvider = ConnectionProvider.builder("levApi")
+      .maxConnections(100)
+      .maxIdleTime(Duration.ofSeconds(20))
+      .maxLifeTime(Duration.ofSeconds(60))
+      .pendingAcquireTimeout(Duration.ofSeconds(60))
+      .evictInBackground(Duration.ofSeconds(120))
+      .build()
+    val httpClient = HttpClient.create(connectionProvider).responseTimeout(Duration.ofSeconds(10))
     return WebClient.builder()
       .baseUrl(levApiRootUri)
       .clientConnector(ReactorClientHttpConnector(httpClient))

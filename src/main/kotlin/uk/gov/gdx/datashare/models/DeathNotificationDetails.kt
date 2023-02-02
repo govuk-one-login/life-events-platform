e375315b7aa895bd177bf5c365c234b2b5a90767
Ethan Mills <ethan.mills@softwire.com>
2023-02-02 12:43:56 +0000

    GPC-145: Change event response to always include allowed fields

--- src/main/kotlin/uk/gov/gdx/datashare/models/DeathNotificationDetails.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/models/DeathNotificationDetails.kt
@@ -1,15 +1,21 @@
 package uk.gov.gdx.datashare.models
 
-import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import io.swagger.v3.oas.annotations.media.Schema
 import org.springframework.format.annotation.DateTimeFormat
+import uk.gov.gdx.datashare.enums.DeathNotificationField
 import uk.gov.gdx.datashare.enums.Sex
+import uk.gov.gdx.datashare.extensions.writeNullableBooleanField
+import uk.gov.gdx.datashare.extensions.writeNullableStringField
 import java.time.LocalDate
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @Schema(description = "Death notification")
+@JsonSerialize(using = DeathNotificationDetailsSerializer::class)
 data class DeathNotificationDetails(
-
+  val enrichmentFields: List<DeathNotificationField>,
   @Schema(description = "Date the death was registered", required = true, example = "2022-01-05", type = "date")
   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
   val registrationDate: LocalDate? = null,
@@ -52,4 +58,77 @@ data class DeathNotificationDetails(
 
   @Schema(description = "Whether the deceased was retired", required = false, example = "false")
   val retired: Boolean? = null,
-)
+) {
+  companion object {
+    fun fromLevDeathRecord(
+      enrichmentFields: List<DeathNotificationField>,
+      levDeathRecord: LevDeathRecord,
+    ): DeathNotificationDetails {
+      with(levDeathRecord) {
+        return DeathNotificationDetails(
+          enrichmentFields = enrichmentFields,
+          registrationDate = if (enrichmentFields.contains(DeathNotificationField.REGISTRATION_DATE)) date else null,
+          firstNames = if (enrichmentFields.contains(DeathNotificationField.FIRST_NAMES)) deceased.forenames else null,
+          lastName = if (enrichmentFields.contains(DeathNotificationField.LAST_NAME)) deceased.surname else null,
+          sex = if (enrichmentFields.contains(DeathNotificationField.SEX)) deceased.sex else null,
+          dateOfDeath = if (enrichmentFields.contains(DeathNotificationField.DATE_OF_DEATH)) deceased.dateOfDeath else null,
+          dateOfBirth = if (enrichmentFields.contains(DeathNotificationField.DATE_OF_BIRTH)) deceased.dateOfBirth else null,
+          birthPlace = if (enrichmentFields.contains(DeathNotificationField.BIRTH_PLACE)) deceased.birthplace else null,
+          deathPlace = if (enrichmentFields.contains(DeathNotificationField.DEATH_PLACE)) deceased.deathplace else null,
+          maidenName = if (enrichmentFields.contains(DeathNotificationField.MAIDEN_NAME)) deceased.maidenSurname else null,
+          occupation = if (enrichmentFields.contains(DeathNotificationField.OCCUPATION)) deceased.occupation else null,
+          retired = if (enrichmentFields.contains(DeathNotificationField.RETIRED)) deceased.retired else null,
+          address = if (enrichmentFields.contains(DeathNotificationField.ADDRESS)) deceased.address else null,
+        )
+      }
+    }
+  }
+}
+
+class DeathNotificationDetailsSerializer :
+  StdSerializer<DeathNotificationDetails>(DeathNotificationDetails::class.java) {
+  override fun serialize(value: DeathNotificationDetails?, gen: JsonGenerator?, provider: SerializerProvider?) {
+    gen?.let {
+      gen.writeStartObject()
+      value?.run {
+        if (enrichmentFields.contains(DeathNotificationField.REGISTRATION_DATE)) {
+          gen.writeNullableStringField(DeathNotificationField.REGISTRATION_DATE.jsonName, registrationDate?.toString())
+        }
+        if (enrichmentFields.contains(DeathNotificationField.FIRST_NAMES)) {
+          gen.writeNullableStringField(DeathNotificationField.FIRST_NAMES.jsonName, firstNames)
+        }
+        if (enrichmentFields.contains(DeathNotificationField.LAST_NAME)) {
+          gen.writeNullableStringField(DeathNotificationField.LAST_NAME.jsonName, lastName)
+        }
+        if (enrichmentFields.contains(DeathNotificationField.SEX)) {
+          gen.writeNullableStringField(DeathNotificationField.SEX.jsonName, sex?.jsonName)
+        }
+        if (enrichmentFields.contains(DeathNotificationField.DATE_OF_DEATH)) {
+          gen.writeNullableStringField(DeathNotificationField.DATE_OF_DEATH.jsonName, dateOfDeath?.toString())
+        }
+        if (enrichmentFields.contains(DeathNotificationField.DATE_OF_BIRTH)) {
+          gen.writeNullableStringField(DeathNotificationField.DATE_OF_BIRTH.jsonName, dateOfBirth?.toString())
+        }
+        if (enrichmentFields.contains(DeathNotificationField.BIRTH_PLACE)) {
+          gen.writeNullableStringField(DeathNotificationField.BIRTH_PLACE.jsonName, birthPlace)
+        }
+        if (enrichmentFields.contains(DeathNotificationField.DEATH_PLACE)) {
+          gen.writeNullableStringField(DeathNotificationField.DEATH_PLACE.jsonName, deathPlace)
+        }
+        if (enrichmentFields.contains(DeathNotificationField.MAIDEN_NAME)) {
+          gen.writeNullableStringField(DeathNotificationField.MAIDEN_NAME.jsonName, maidenName)
+        }
+        if (enrichmentFields.contains(DeathNotificationField.OCCUPATION)) {
+          gen.writeNullableStringField(DeathNotificationField.OCCUPATION.jsonName, occupation)
+        }
+        if (enrichmentFields.contains(DeathNotificationField.RETIRED)) {
+          gen.writeNullableBooleanField(DeathNotificationField.RETIRED.jsonName, retired)
+        }
+        if (enrichmentFields.contains(DeathNotificationField.ADDRESS)) {
+          gen.writeNullableStringField(DeathNotificationField.ADDRESS.jsonName, address)
+        }
+      }
+      gen.writeEndObject()
+    }
+  }
+}
--- src/main/kotlin/uk/gov/gdx/datashare/services/DeathNotificationService.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/services/DeathNotificationService.kt
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.gdx.datashare.enums.DeathNotificationField
 import uk.gov.gdx.datashare.models.DeathNotificationDetails
-import uk.gov.gdx.datashare.models.LevDeathRecord
 
 @Service
 @XRayEnabled
@@ -22,30 +21,9 @@ class DeathNotificationService(
     enrichmentFields: List<DeathNotificationField>,
   ): DeathNotificationDetails? {
     val citizenDeathId = dataId.toInt()
-    return levApiService.findDeathById(citizenDeathId)
-      .map {
-        mapDeathNotificationDetails(it, enrichmentFields)
-      }.first()
-  }
-
-  private fun mapDeathNotificationDetails(
-    deathRecord: LevDeathRecord,
-    ef: List<DeathNotificationField>,
-  ): DeathNotificationDetails {
-    val deceased = deathRecord.deceased
-    return DeathNotificationDetails(
-      registrationDate = if (ef.contains(DeathNotificationField.REGISTRATION_DATE)) deathRecord.date else null,
-      firstNames = if (ef.contains(DeathNotificationField.FIRST_NAMES)) deceased.forenames else null,
-      lastName = if (ef.contains(DeathNotificationField.LAST_NAME)) deceased.surname else null,
-      sex = if (ef.contains(DeathNotificationField.SEX)) deceased.sex else null,
-      dateOfDeath = if (ef.contains(DeathNotificationField.DATE_OF_DEATH)) deceased.dateOfDeath else null,
-      dateOfBirth = if (ef.contains(DeathNotificationField.DATE_OF_BIRTH)) deceased.dateOfBirth else null,
-      birthPlace = if (ef.contains(DeathNotificationField.BIRTH_PLACE)) deceased.birthplace else null,
-      deathPlace = if (ef.contains(DeathNotificationField.DEATH_PLACE)) deceased.deathplace else null,
-      maidenName = if (ef.contains(DeathNotificationField.MAIDEN_NAME)) deceased.maidenSurname else null,
-      occupation = if (ef.contains(DeathNotificationField.OCCUPATION)) deceased.occupation else null,
-      retired = if (ef.contains(DeathNotificationField.RETIRED)) deceased.retired else null,
-      address = if (ef.contains(DeathNotificationField.ADDRESS)) deceased.address else null,
-    )
+    return levApiService
+      .findDeathById(citizenDeathId)
+      .map { DeathNotificationDetails.fromLevDeathRecord(enrichmentFields, it) }
+      .first()
   }
 }

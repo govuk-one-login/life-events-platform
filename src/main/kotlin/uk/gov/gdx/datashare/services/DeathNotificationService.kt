6ec47635e9839f0893df0b7464b335459fc1eeb5
CarGil <carly.gilson@softwire.com>
2023-02-02 10:11:08 +0000

    GPC-143: Update enrichment to use enums

--- src/main/kotlin/uk/gov/gdx/datashare/services/DeathNotificationService.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/services/DeathNotificationService.kt
@@ -1,17 +1,17 @@
 package uk.gov.gdx.datashare.services
 
 import com.amazonaws.xray.spring.aop.XRayEnabled
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import uk.gov.gdx.datashare.enums.DeathNotificationField
 import uk.gov.gdx.datashare.models.DeathNotificationDetails
+import uk.gov.gdx.datashare.models.LevDeathRecord
 
 @Service
 @XRayEnabled
 class DeathNotificationService(
   private val levApiService: LevApiService,
-  private val objectMapper: ObjectMapper,
 ) {
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
@@ -19,27 +19,33 @@ class DeathNotificationService(
 
   fun getEnrichedPayload(
     dataId: String,
-    enrichmentFields: List<String>,
+    enrichmentFields: List<DeathNotificationField>,
   ): DeathNotificationDetails? {
     val citizenDeathId = dataId.toInt()
-    val allEnrichedData = levApiService.findDeathById(citizenDeathId)
+    return levApiService.findDeathById(citizenDeathId)
       .map {
-        DeathNotificationDetails(
-          registrationDate = it.date,
-          firstNames = it.deceased.forenames,
-          lastName = it.deceased.surname,
-          sex = it.deceased.sex,
-          dateOfDeath = it.deceased.dateOfDeath,
-          dateOfBirth = it.deceased.dateOfBirth,
-          birthPlace = it.deceased.birthplace,
-          deathPlace = it.deceased.deathplace,
-          maidenName = it.deceased.maidenSurname,
-          occupation = it.deceased.occupation,
-          retired = it.deceased.retired,
-          address = it.deceased.address,
-        )
+        mapDeathNotificationDetails(it, enrichmentFields)
       }.first()
+  }
 
-    return EnrichmentService.getDataWithOnlyFields(objectMapper, allEnrichedData, enrichmentFields)
+  private fun mapDeathNotificationDetails(
+    deathRecord: LevDeathRecord,
+    ef: List<DeathNotificationField>,
+  ): DeathNotificationDetails {
+    val deceased = deathRecord.deceased
+    return DeathNotificationDetails(
+      registrationDate = if (ef.contains(DeathNotificationField.REGISTRATION_DATE)) deathRecord.date else null,
+      firstNames = if (ef.contains(DeathNotificationField.FIRST_NAMES)) deceased.forenames else null,
+      lastName = if (ef.contains(DeathNotificationField.LAST_NAME)) deceased.surname else null,
+      sex = if (ef.contains(DeathNotificationField.SEX)) deceased.sex else null,
+      dateOfDeath = if (ef.contains(DeathNotificationField.DATE_OF_DEATH)) deceased.dateOfDeath else null,
+      dateOfBirth = if (ef.contains(DeathNotificationField.DATE_OF_BIRTH)) deceased.dateOfBirth else null,
+      birthPlace = if (ef.contains(DeathNotificationField.BIRTH_PLACE)) deceased.birthplace else null,
+      deathPlace = if (ef.contains(DeathNotificationField.DEATH_PLACE)) deceased.deathplace else null,
+      maidenName = if (ef.contains(DeathNotificationField.MAIDEN_NAME)) deceased.maidenSurname else null,
+      occupation = if (ef.contains(DeathNotificationField.OCCUPATION)) deceased.occupation else null,
+      retired = if (ef.contains(DeathNotificationField.RETIRED)) deceased.retired else null,
+      address = if (ef.contains(DeathNotificationField.ADDRESS)) deceased.address else null,
+    )
   }
 }

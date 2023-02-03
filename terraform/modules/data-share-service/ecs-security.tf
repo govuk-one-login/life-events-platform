06413b139493cb12293152f0b38404b9987c87cc
Oskar Williams <oskar.williams@softwire.com>
2023-02-03 10:33:23 +0000

    GPC-95: Restrict ECS egress to HTTPS

--- terraform/modules/data-share-service/ecs-security.tf
+++ terraform/modules/data-share-service/ecs-security.tf
@@ -13,9 +13,9 @@ resource "aws_security_group" "ecs_tasks" {
   }
 
   egress {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
     description = "ECS task egress rule"
   }

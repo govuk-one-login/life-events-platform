a20c8abecb01b955e26ccacc88f69bf2ad4e3d54
CarGil <carly.gilson@softwire.com>
2023-02-07 11:06:24 +0000

    GPC-119: Configure Javers

--- src/main/kotlin/uk/gov/gdx/datashare/repositories/AcquirerSubscriptionEnrichmentFieldRepository.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/repositories/AcquirerSubscriptionEnrichmentFieldRepository.kt
@@ -1,10 +1,12 @@
 package uk.gov.gdx.datashare.repositories
 
+import org.javers.spring.annotation.JaversSpringDataAuditable
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
+@JaversSpringDataAuditable
 interface AcquirerSubscriptionEnrichmentFieldRepository : CrudRepository<AcquirerSubscriptionEnrichmentField, UUID> {
   fun findAllByAcquirerSubscriptionId(acquirerSubscriptionId: UUID): List<AcquirerSubscriptionEnrichmentField>
 

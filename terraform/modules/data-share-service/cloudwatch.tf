00c0b994f221be2fd7d74ebe7bed46f6515dfe9c
Oskar Williams <oskar.williams@softwire.com>
2023-02-08 15:43:39 +0000

    GPC-NONE: Update cloudfront sg rule to use managed aws prefix list (#455)
    GPC-NONE: Automatically reformat terraform files

--- terraform/modules/data-share-service/cloudwatch.tf
+++ terraform/modules/data-share-service/cloudwatch.tf
@@ -15,13 +15,6 @@ resource "aws_cloudwatch_log_group" "ecs_logs" {
   kms_key_id = aws_kms_key.log_key.arn
 }
 
-resource "aws_cloudwatch_log_group" "lb_sg_update" {
-  name              = "/aws/lambda/${aws_lambda_function.lb_sg_update.function_name}"
-  retention_in_days = var.cloudwatch_retention_period
-
-  kms_key_id = aws_kms_key.log_key.arn
-}
-
 module "metrics_dashboard" {
   source           = "../cloudwatch_dashboard"
   region           = var.region

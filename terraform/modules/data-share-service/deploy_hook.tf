8780cc562b3325c2ca8a92abf9f16c111fe61c36
Oskar Williams <oskar.williams@softwire.com>
2023-02-09 17:21:27 +0000

    GPC-NONE: Update lambda SG to allow for test

--- terraform/modules/data-share-service/deploy_hook.tf
+++ terraform/modules/data-share-service/deploy_hook.tf
@@ -35,3 +35,14 @@ resource "aws_security_group_rule" "lambda_https" {
   description       = "Lambda security group egress rule for HTTPS"
   security_group_id = aws_security_group.lambda.id
 }
+
+#tfsec:ignore:aws-ec2-no-public-egress-sgr
+resource "aws_security_group_rule" "lambda_test" {
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = 8080
+  to_port           = 8080
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Lambda security group egress rule for connecting to test GDX port"
+  security_group_id = aws_security_group.lambda.id
+}

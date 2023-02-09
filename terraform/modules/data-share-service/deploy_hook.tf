c9ce0fcc0e9411de6ef6844cf43a087a018248e4
Oskar Williams <oskar.williams@softwire.com>
2023-02-09 12:19:56 +0000

    GPC-93: Make the test lb listener public  not through cloudfront (#471)
    * GPC-93: Make the test lb listener public  not through cloudfront
    
    * GPC-NONE: Automatically reformat terraform files
    
    * GPC-93: tfsec
    
    ---------
    
    Co-authored-by: oskarwilliams378 <oskarwilliams378@users.noreply.github.com>

--- terraform/modules/data-share-service/deploy_hook.tf
+++ terraform/modules/data-share-service/deploy_hook.tf
@@ -5,7 +5,7 @@ module "deploy_hook" {
   cloudwatch_retention_period = var.cloudwatch_retention_period
   codedeploy_arn              = aws_codedeploy_deployment_group.gdx_data_share_poc.arn
 
-  test_gdx_url     = "${local.gdx_api_base_url}:8443"
+  test_gdx_url     = "http://${aws_lb.load_balancer.dns_name}:8080"
   test_auth_header = random_password.test_auth_header.result
   auth_url         = module.cognito.token_auth_url
   client_id        = module.cognito.deploy_hook_client_id

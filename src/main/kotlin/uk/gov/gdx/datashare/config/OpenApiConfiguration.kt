6605605241bf0efabd1b94dcb09ca4e7c7a5f104
Michael Willis <mike.willis@digital.cabinet-office.gov.uk>
2023-02-13 17:04:03 +0000

    GPC-153 Linting fixes

--- src/main/kotlin/uk/gov/gdx/datashare/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/config/OpenApiConfiguration.kt
@@ -4,12 +4,14 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
 import io.swagger.v3.oas.models.media.DateTimeSchema
 import io.swagger.v3.oas.models.media.Schema
 import io.swagger.v3.oas.models.media.StringSchema
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
+import io.swagger.v3.oas.models.tags.Tag
 import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.info.BuildProperties
@@ -30,11 +32,27 @@ class OpenApiConfiguration(
         Server().url("/").description("Current url"),
       ),
     )
+    .tags(
+      listOf(
+        Tag().name("01. Acquirer").description("Acquiring events from the platform"),
+        Tag().name("02. Supplier").description("Supplying events to the platform"),
+        Tag().name("11. Acquirers").description("Maintenance of Acquirers"),
+        Tag().name("12. Suppliers").description("Maintenance of Suppliers"),
+        Tag().name("13. Admin").description("Admin functions"),
+        Tag().name("20. Data").description("Maintenance of Data"),
+      ),
+    )
     .info(
       Info().title("GDX Data Share API")
         .version(version)
+        .license(License().name("MIT").url("https://opensource.org/license/mit-0"))
         .description("API for obtaining data about citizen life events")
-        .contact(Contact().name("GDX Vision Team").email("mike.willis@digital.cabinet-office.gov.uk")),
+        .contact(
+          Contact()
+            .name("GDX Platform Team")
+            .email("gdx-dev-team@digital.cabinet-office.gov.uk")
+            .url("https://www.gov.uk/government/organisations/government-digital-service"),
+        ),
     )
     .components(
       Components().addSecuritySchemes(

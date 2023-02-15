a3cb7f37dbb25e86b2b49edc2167e252c7a5e5e1
CarGil <carly.gilson@softwire.com>
2023-02-15 10:35:01 +0000

    GPC-146: Add encryption to topic

--- terraform/modules/data-share-service/sns_alarm.tf
+++ terraform/modules/data-share-service/sns_alarm.tf
@@ -2,8 +2,19 @@ locals {
   notification_emails = []
 }
 
+resource "aws_kms_key" "sns_alarm_key" {
+  enable_key_rotation = true
+  description         = "Key used to encrypt sns alarm"
+}
+
+resource "aws_kms_alias" "sqs_key_alias" {
+  name          = "alias/${var.environment}/sns-alarm-key"
+  target_key_id = aws_kms_key.sns_alarm_key.arn
+}
+
 resource "aws_sns_topic" "sns_alarm_topic" {
-  name = "${var.environment}-gdx-alarms"
+  name              = "${var.environment}-gdx-alarms"
+  kms_master_key_id = aws_kms_key.sns_alarm_key.arn
 }
 
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {

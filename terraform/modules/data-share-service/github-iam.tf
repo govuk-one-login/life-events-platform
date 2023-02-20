4515608e5ad1d02442f82ca372e49b7452e35eb4
Oskar Williams <oskar.williams@softwire.com>
2023-02-20 11:36:48 +0000

    GPC-191: Fix permissions

--- terraform/modules/data-share-service/github-iam.tf
+++ terraform/modules/data-share-service/github-iam.tf
@@ -87,6 +87,15 @@ data "aws_iam_policy_document" "github_oidc_pull_request_state" {
     ]
     effect = "Allow"
   }
+  statement {
+    actions = [
+      "aps:DescribeLoggingConfiguration"
+    ]
+    resources = [
+      aws_prometheus_workspace.prometheus.arn
+    ]
+    effect = "Allow"
+  }
 }
 
 resource "aws_iam_policy" "github_oidc_pull_request_state" {

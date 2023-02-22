0a8cc7dd91343cd626acf7a4c9627e944fcaba30
Oskar Williams <oskar.williams@softwire.com>
2023-02-22 14:23:34 +0000

    GPC-NONE: Add prometheus sns permissions

--- terraform/modules/data-share-service/sns_alarm.tf
+++ terraform/modules/data-share-service/sns_alarm.tf
@@ -1,5 +1,7 @@
 locals {
   notification_emails = ["gdx-dev-team@digital.cabinet-office.gov.uk"]
+
+
 }
 
 resource "aws_kms_key" "sns_alarm_key" {
@@ -15,6 +17,46 @@ resource "aws_kms_alias" "sqs_key_alias" {
 resource "aws_sns_topic" "sns_alarm_topic" {
   name              = "${var.environment}-gdx-alarms"
   kms_master_key_id = aws_kms_key.sns_alarm_key.arn
+}
+
+data "aws_iam_policy_document" "sns_access" {
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = [aws_sns_topic.sns_alarm_topic.arn]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [data.aws_caller_identity.current.account_id]
+    }
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+
+      values = [aws_prometheus_workspace.prometheus.arn]
+    }
+  }
+}
+
+resource "aws_sns_topic_policy" "default" {
+  arn = aws_sns_topic.sns_alarm_topic.arn
+
+  policy = data.aws_iam_policy_document.sns_access.json
 }
 
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {

598695b418628b3904ef280db91513f392f3ec10
Olly Levett <oliver.levett@softwire.com>
2023-03-15 15:57:33 +0000

    GPC-248: Enable cross region aggregation

--- terraform/modules/security_hub/main.tf
+++ terraform/modules/security_hub/main.tf
@@ -4,3 +4,8 @@ resource "aws_securityhub_standards_subscription" "cis" {
   depends_on    = [aws_securityhub_account.securityhub]
   standards_arn = "arn:aws:securityhub:${var.region}::standards/cis-aws-foundations-benchmark/v/1.4.0"
 }
+
+resource "aws_securityhub_finding_aggregator" "securityhub" {
+  depends_on   = [aws_securityhub_account.securityhub]
+  linking_mode = "ALL_REGIONS"
+}

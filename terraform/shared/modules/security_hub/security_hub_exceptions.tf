43489f9d6fcb540ff5a6ecf1e1a78e8315c28129
Oskar Williams <oskar.williams@softwire.com>
2023-03-16 15:41:26 +0000

    GPC-254: Fix for_each

--- terraform/shared/modules/security_hub/security_hub_exceptions.tf
+++ terraform/shared/modules/security_hub/security_hub_exceptions.tf
@@ -1,5 +1,8 @@
 resource "aws_securityhub_standards_control" "hardware_mfa" {
-  for_each              = var.rules
+  for_each = {
+    for index, rule in var.rules :
+    index => rule
+  }
   standards_control_arn = "arn:aws:config:eu-west-2:${var.account_id}:${each.value.rule}"
   control_status        = "DISABLED"
   disabled_reason       = each.value.disabled_reason

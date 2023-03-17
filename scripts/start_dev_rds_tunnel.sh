70801df2fb5a1705fd27ce40d59fcb4e7d2bdbc3
CarlyG55 <carly.gilson@softwire.com>
2023-03-17 14:11:29 +0000

    GPC-189: Replace SSH bastion hosts with SSM (#670)
    * GPC-189: Replace SSH bastion hosts with SSM
    
    * GPC-189: Fix security group egress
    
    * GPC-189: Add vpc endpoint for privatelink
    
    * GPC-189: Add scripts and run config for rds tunnels
    
    * GPC-189: Update docs for connecting to hosted databases
    
    * Update terraform/modules/data-share-service/ec2.tf
    
    Co-authored-by: Ethan Mills <ethan.mills@softwire.com>
    
    * Update terraform/modules/data-share-service/ec2.tf
    
    Co-authored-by: Ethan Mills <ethan.mills@softwire.com>
    
    * Update terraform/modules/data-share-service/ec2.tf
    
    Co-authored-by: Ethan Mills <ethan.mills@softwire.com>
    
    ---------
    
    Co-authored-by: Ethan Mills <ethan.mills@softwire.com>

---
+++ scripts/start_dev_rds_tunnel.sh
@@ -0,0 +1,6 @@
+EC2_INSTANCE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=dev-rds-bastion-host" "Name=instance-state-name,Values=running" | jq '.Reservations[].Instances[].InstanceId')
+
+aws ssm start-session \
+    --target $EC2_INSTANCE \
+    --document-name AWS-StartPortForwardingSessionToRemoteHost \
+    --parameters host="dev-rds-db.cluster-chfyfnfoqhf6.eu-west-2.rds.amazonaws.com",portNumber="5432",localPortNumber="5433"

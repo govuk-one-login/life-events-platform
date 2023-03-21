62c2b525abd6cb2ac619ccd797d62f563cd24632
Oskar Williams <oskar.williams@softwire.com>
2023-03-21 14:46:06 +0000

    GPC-NONE: Remove unneeded s3 policies (#734)
    * GPC-NONE: Remove unneeded s3 policies
    
    * GPC-NONE: Change to just being adding a file

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -6,24 +6,9 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-# This is the logging bucket, it doesn't need logs or versioning
-#tfsec:ignore:aws-s3-enable-bucket-logging
-#tfsec:ignore:aws-s3-enable-versioning
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "${var.environment}-${var.name}-logs-gdx-data-share-poc"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
 resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
-}
-
-resource "aws_s3_bucket_acl" "log_bucket_acl" {
-  bucket = aws_s3_bucket.log_bucket.id
-  acl    = "log-delivery-write"
 }
 
 resource "aws_s3_bucket_versioning" "bucket" {
@@ -54,35 +39,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
-  bucket = aws_s3_bucket.log_bucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.bucket.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_public_access_block" "log_bucket" {
-  bucket = aws_s3_bucket.log_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket_logging" "bucket_logging" {
   bucket = aws_s3_bucket.bucket.id
 
@@ -101,4 +57,13 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
     }
     status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }

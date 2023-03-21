8c0a0cb280062406ab45a8746f8fe987280834f5
Oskar Williams <oskar.williams@softwire.com>
2023-03-21 14:29:24 +0000

    GPC-NONE: Change to just being adding a file

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -58,3 +58,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}

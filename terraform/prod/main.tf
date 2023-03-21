55ae4e5885b5a7882868b14dab82de5fc31432a0
Oskar Williams <oskar.williams@softwire.com>
2023-03-21 15:41:29 +0000

    GPC-NONE: Add prod main

---
+++ terraform/prod/main.tf
@@ -0,0 +1,28 @@
+locals {
+  env = "prod"
+  default_tags = {
+    Product     = "Government Data Exchange"
+    Environment = local.env
+    Owner       = "gdx-dev-team@digital.cabinet-office.gov.uk"
+    Source      = "terraform"
+    Repository  = "https://github.com/alphagov/gdx-data-share-poc"
+  }
+}
+
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "gdx-data-share-tfstate"
+    key            = "terraform-prod.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "gdx-data-share-lock"
+    encrypt        = true
+  }
+}

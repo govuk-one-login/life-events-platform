012ccc21293b4df7af66037127d0a2b629f2ea8f
Oskar Williams <oskar.williams@softwire.com>
2023-03-21 16:02:20 +0000

    GPC-NONE: Deny insecure transport to buckets (#735)

--- terraform/modules/bootstrap/bootstrap.tf
+++ terraform/modules/bootstrap/bootstrap.tf
@@ -64,3 +64,37 @@ resource "aws_dynamodb_table" "tf_lock_state" {
     type = "S"
   }
 }
+
+data "aws_iam_policy_document" "deny_insecure_transport" {
+  statement {
+    sid    = "DenyInsecureTransport"
+    effect = "Deny"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      aws_s3_bucket.state_bucket.arn,
+      "${aws_s3_bucket.state_bucket.arn}/*",
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values = [
+        "false"
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "deny_insecure_transport" {
+  bucket = aws_s3_bucket.state_bucket.id
+  policy = data.aws_iam_policy_document.deny_insecure_transport.json
+}

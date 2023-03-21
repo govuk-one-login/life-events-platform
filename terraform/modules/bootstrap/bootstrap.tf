0462bbf3d60aceddc2daaebba7de909a28a9680b
Oskar Williams <oskar.williams@softwire.com>
2023-03-21 11:07:37 +0000

    GPC-NONE: Format

--- terraform/modules/bootstrap/bootstrap.tf
+++ terraform/modules/bootstrap/bootstrap.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_acl" "state_bucket" {
 resource "aws_s3_bucket_versioning" "state_bucket" {
   bucket = aws_s3_bucket.state_bucket.id
   versioning_configuration {
-    status     = "Enabled"
+    status = "Enabled"
   }
 }
 

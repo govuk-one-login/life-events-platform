6f2dcd175661295e8761f64fe458f706d1994b5d
Oskar Williams <oskar.williams@softwire.com>
2023-03-24 09:55:26 +0000

    GPC-NONE: Update db scripts to allow for profiles (#760)

--- scripts/start_demo_rds_tunnel.sh
+++ scripts/start_demo_rds_tunnel.sh
@@ -1,3 +1,8 @@
+if [ $# -eq 1 ];
+then
+  export AWS_PROFILE=$1
+fi;
+
 EC2_INSTANCE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=demo-rds-bastion-host" "Name=instance-state-name,Values=running" | jq -r '.Reservations[].Instances[].InstanceId')
 
 aws ssm start-session \

0d9498d45cc111ddc7030b22e0b0a97d462c62c0
Oskar Williams <oskar.williams@softwire.com>
2023-03-31 11:38:49 +0100

    GPC-NONE: Add gradle lockfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+dependencyLocking {
+  lockAllConfigurations()
+}
+
 plugins {
   id("org.springframework.boot") version "3.0.5"
   id("io.spring.dependency-management") version "1.1.0"

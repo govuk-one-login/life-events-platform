3ceb9b22fad8ffd61e5345aecf86f7984e94425a
Oskar Williams <oskar.williams@softwire.com>
2023-04-11 16:51:46 +0100

    GPC-NONE: Max password age

--- terraform/modules/policies/iam.tf
+++ terraform/modules/policies/iam.tf
@@ -43,4 +43,5 @@ resource "aws_iam_account_password_policy" "password_policy" {
   require_symbols                = true
   allow_users_to_change_password = true
   password_reuse_prevention      = 24
+  max_password_age               = 90
 }

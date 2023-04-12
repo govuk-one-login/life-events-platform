39cea4f4823cfe94acc42aa79156925621f9a38a
Oskar Williams <oskar.williams@softwire.com>
2023-04-12 09:24:26 +0100

    GPC-NONE: Add IAM password policy (#908)
    * GPC-NONE: Add IAM password policy
    
    * GPC-NONE: Max password age

--- terraform/modules/policies/iam.tf
+++ terraform/modules/policies/iam.tf
@@ -34,3 +34,14 @@ resource "aws_iam_group_policy_attachment" "enforce_mfa" {
   group      = aws_iam_group.mfa_enforced.name
   policy_arn = aws_iam_policy.enforce_mfa.arn
 }
+
+resource "aws_iam_account_password_policy" "password_policy" {
+  minimum_password_length        = 16
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  password_reuse_prevention      = 24
+  max_password_age               = 90
+}

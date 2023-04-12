93b2d479d54e047f34583e4d597c506e373bb884
Oskar Williams <oskar.williams@softwire.com>
2023-04-12 09:22:21 +0100

    GPC-307: Add access logging to LBs (#906)

--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -4,11 +4,16 @@ variable "expiration_days" {
   type    = number
   default = null
 }
-variable "kms_key_policy_json" {
-  type    = string
-  default = null
-}
+
 variable "allow_logs" {
   type    = bool
   default = false
+}
+variable "account_id" {
+  type    = string
+  default = ""
+}
+variable "region" {
+  type    = string
+  default = ""
 }

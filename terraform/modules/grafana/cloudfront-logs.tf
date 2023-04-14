39a9896dd9924aa5eaa7ead653422f396ee151b3
Oskar Williams <oskar.williams@softwire.com>
2023-04-14 12:50:59 +0100

    GPC-NONE: Fix typo

--- terraform/modules/grafana/cloudfront-logs.tf
+++ terraform/modules/grafana/cloudfront-logs.tf
@@ -6,7 +6,7 @@ module "cloudfront_logs_bucket" {
 
   account_id      = var.account_id
   region          = var.region
-  environment     = grafana
+  environment     = "grafana"
   name            = "cloudfront-logs"
   expiration_days = 180
 

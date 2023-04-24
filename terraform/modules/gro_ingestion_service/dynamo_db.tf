e4fccad208114f2d505e3360283cd946bf8dfd86
Oskar Williams <oskar.williams@softwire.com>
2023-04-24 16:18:47 +0100

    GPC-348: Add dynamo db table (#1033)
    * GPC-348: Add dynamo db table
    
    * GPC-348: Specify environment
    
    * GPC-348: Fix trivy
    
    * GPC-348: Add attributes for dynamoDB
    
    * GPC-348: Add registration ID
    
    * GPC-348: Remove attributes
    
    * GPC-348: Fix typo

---
+++ terraform/modules/gro_ingestion_service/dynamo_db.tf
@@ -0,0 +1,28 @@
+#tfsec:ignore:aws-dynamodb-enable-recovery
+resource "aws_dynamodb_table" "gro_ingestion" {
+  name             = "${var.environment}_gro_ingestion"
+  billing_mode     = "PAY_PER_REQUEST"
+  hash_key         = "hash"
+  stream_enabled   = true
+  stream_view_type = "KEYS_ONLY"
+
+  attribute {
+    name = "hash"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.gro_ingestion.arn
+  }
+}
+
+resource "aws_kms_key" "gro_ingestion" {
+  description         = "Encryption key for GRO ingestion"
+  enable_key_rotation = true
+}
+
+resource "aws_kms_alias" "gro_ingestion" {
+  name          = "alias/${var.environment}/gro-ingestion-key"
+  target_key_id = aws_kms_key.gro_ingestion.arn
+}

a494226afdc5cffe0fa2ded08affc6de7010ba75
Oskar Williams <oskar.williams@softwire.com>
2023-04-24 15:20:34 +0100

    GPC-348: Add attributes for dynamoDB

--- terraform/modules/gro_ingestion_service/dynamo_db.tf
+++ terraform/modules/gro_ingestion_service/dynamo_db.tf
@@ -11,6 +11,76 @@ resource "aws_dynamodb_table" "gor_ingestion" {
     type = "S"
   }
 
+  attribute {
+    name = "FirstForename"
+    type = "S"
+  }
+
+  attribute {
+    name = "Surname"
+    type = "S"
+  }
+
+  attribute {
+    name = "MaidenSurname"
+    type = "S"
+  }
+
+  attribute {
+    name = "Sex"
+    type = "S"
+  }
+
+  attribute {
+    name = "DateOfDeath"
+    type = "S"
+  }
+
+  attribute {
+    name = "DateOfBirth"
+    type = "S"
+  }
+
+  attribute {
+    name = "AddressLine1"
+    type = "S"
+  }
+
+  attribute {
+    name = "AddressLine2"
+    type = "S"
+  }
+
+  attribute {
+    name = "AddressLine3"
+    type = "S"
+  }
+
+  attribute {
+    name = "AddressLine4"
+    type = "S"
+  }
+
+  attribute {
+    name = "Postcode"
+    type = "S"
+  }
+
+  attribute {
+    name = "VerificationLevelTypes"
+    type = "N"
+  }
+
+  attribute {
+    name = "PartialMonthOfDeath"
+    type = "N"
+  }
+
+  attribute {
+    name = "PartialYearOfDeath"
+    type = "N"
+  }
+
   server_side_encryption {
     enabled     = true
     kms_key_arn = aws_kms_key.gor_ingestion.arn

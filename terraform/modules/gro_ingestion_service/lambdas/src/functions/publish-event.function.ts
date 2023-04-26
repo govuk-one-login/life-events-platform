fe6433775e7976c30d52fb154fed2335a5c80aac
Oskar Williams <oskar.williams@softwire.com>
2023-04-26 12:05:10 +0100

    GPC-349: Add in auth

--- terraform/modules/gro_ingestion_service/lambdas/src/functions/publish-event.function.ts
+++ terraform/modules/gro_ingestion_service/lambdas/src/functions/publish-event.function.ts
@@ -1,30 +1,15 @@
 import { DynamoDBStreamEvent, Handler } from "aws-lambda"
 import { mapToEventRecord } from "../models/EventRecord"
-import { request } from "https"
+import { request, RequestOptions } from "https"
 import { PublishEvent } from "../models/PublishEvent"
 import { AttributeValue } from "aws-lambda/trigger/dynamodb-stream"
 
 const apiUrl = process.env.API_URL
-const options = {
-    hostname: apiUrl,
-    path: "/events",
-    method: "POST",
-    port: 443,
-    headers: {
-        "Content-Type": "application/json",
-    },
-}
+const authUrl = process.env.AUTH_URL
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
 
-const convertToEvent = (image: { [key: string]: AttributeValue }): PublishEvent => {
-    const eventRecord = mapToEventRecord(image)
-    return {
-        eventType: "DEATH_NOTIFICATION",
-        eventTime: eventRecord.EventTime,
-        id: eventRecord.hash
-    }
-}
-
-const publishEvent = async (event: PublishEvent) => new Promise((resolve, reject) => {
+const makeRequest = async (options: RequestOptions, requestData: string): Promise<object> => new Promise((resolve, reject) => {
     const req = request(options, res => {
         res.setEncoding("utf8")
         let responseBody = ""
@@ -37,7 +22,7 @@ const publishEvent = async (event: PublishEvent) => new Promise((resolve, reject
             try {
                 resolve(JSON.parse(responseBody))
             } catch (err) {
-                reject({ error: err, event: event })
+                reject(err)
             }
         })
     })
@@ -45,9 +30,57 @@ const publishEvent = async (event: PublishEvent) => new Promise((resolve, reject
         reject(err)
     })
 
-    req.write(event)
+    req.write(requestData)
     req.end()
 })
+
+const getAccessToken = async () => {
+    const authRequest = JSON.stringify({
+        grant_type: "client_credentials",
+        client_id: clientId,
+        client_secret: clientSecret
+    })
+
+    const authOptions: RequestOptions = {
+        path: authUrl,
+        method: "POST",
+        port: 443,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Length": authRequest.length
+        },
+    }
+
+    const result = await makeRequest(authOptions, authRequest)
+    return result["access_token"]
+}
+
+const convertToEvent = (image: { [key: string]: AttributeValue }): PublishEvent => {
+    const eventRecord = mapToEventRecord(image)
+    return {
+        eventType: "DEATH_NOTIFICATION",
+        eventTime: eventRecord.EventTime,
+        id: eventRecord.hash
+    }
+}
+
+const publishEvent = async (event: PublishEvent, accessToken: string) => {
+    const eventData = JSON.stringify(event)
+
+    const options: RequestOptions = {
+        hostname: apiUrl,
+        path: "/events",
+        method: "POST",
+        port: 443,
+        headers: {
+            "Authorization": `Bearer ${accessToken}`,
+            "Content-Type": "application/json; charset=utf-8",
+            "Content-Length": eventData.length
+        },
+    }
+
+    await makeRequest(options, eventData)
+}
 
 export const handler: Handler = async (event: DynamoDBStreamEvent) => {
     const publishEvents = event.Records
@@ -55,9 +88,15 @@ export const handler: Handler = async (event: DynamoDBStreamEvent) => {
         .map(r => r.dynamodb?.NewImage)
         .map(convertToEvent)
 
+    const accessToken = await getAccessToken()
+
     console.log(`Publishing ${publishEvents.length} events`)
-    const results = await Promise.allSettled(publishEvents.map(publishEvent))
-    const failedEvents = results.filter((r): r is PromiseRejectedResult => r.status === "rejected")
+    const results = await Promise.allSettled(
+        publishEvents.map(e => publishEvent(e, accessToken))
+    )
+    const failedEvents = results
+        .filter((r): r is PromiseRejectedResult => r.status === "rejected")
+
     if (failedEvents.length > 0) {
         throw new Error(`Failed to publish ${failedEvents.length} events`)
     }

20e104e40698edf14a4196cafe077d6b01c674e8
Mjwillis <Mjwillis@users.noreply.github.com>
2023-05-03 14:16:31 +0000

    GPC-NONE: Automatically lint files

--- terraform/modules/gro_ingestion_service/lambdas/test/functions/deleteEvent.function.spec.ts
+++ terraform/modules/gro_ingestion_service/lambdas/test/functions/deleteEvent.function.spec.ts
@@ -1,10 +1,10 @@
 import { describe, expect } from "@jest/globals"
 
+import { handler } from "../../src/functions/deleteEvent.function"
+import { DeleteEventResponse } from "../../src/models/DeleteEventResponse"
 import { EventRequest } from "../../src/models/EventRequest"
 import { DynamoDBClient, dynamoDbSendFn } from "../__mocks__/@aws-sdk/client-dynamodb"
 import { dbItem } from "../const/dbItem"
-import { DeleteEventResponse } from "../../src/models/DeleteEventResponse"
-import { handler } from "../../src/functions/deleteEvent.function"
 import { eventRequest } from "../const/eventRequest"
 
 const db = new DynamoDBClient()

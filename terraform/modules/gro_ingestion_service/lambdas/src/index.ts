9942d9e0199c6f805542f664975d0147e16f44dc
Oskar Williams <oskar.williams@softwire.com>
2023-05-04 13:35:19 +0100

    GPC-NONE: Fix lambda perms (#1098)

--- terraform/modules/gro_ingestion_service/lambdas/src/index.ts
+++ terraform/modules/gro_ingestion_service/lambdas/src/index.ts
@@ -1,17 +1,9 @@
 import { Handler } from "aws-lambda"
 
-import deleteEvent from "./functions/deleteEvent"
-import deleteXml from "./functions/deleteXml"
-import enrichEvent from "./functions/enrichEvent"
-import mapXml from "./functions/mapXml"
-import publishEvent from "./functions/publishEvent"
-import splitXml from "./functions/splitXml"
-import { LambdaFunction } from "./models/LambdaFunction"
+import { functions } from "./functions"
 
 export const handler: Handler = async (event, context, callback) => {
     const functionName = process.env.FUNCTION_NAME ?? "-"
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const functions: LambdaFunction[] = [deleteEvent, deleteXml, enrichEvent, mapXml, publishEvent, splitXml]
 
     const lambdaFunction = functions.find(f => f.name === functionName)
     if (!lambdaFunction) {

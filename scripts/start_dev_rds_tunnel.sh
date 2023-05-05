f3f95a468536fbb19e5cd3c831ac834e9e70cec0
Oskar Williams <oskar.williams@softwire.com>
2023-05-05 10:16:15 +0100

    GPC-NONE: Fix dev tunneling

--- scripts/start_dev_rds_tunnel.sh
+++ scripts/start_dev_rds_tunnel.sh
@@ -1,6 +1,6 @@
 eval $(assume-role gdx)
 
-EC2_INSTANCE=$(aws ec2 describe-instances --region eu-west-2 --filters "Name=tag:Name,Values=dev-rds-bastion-host" "Name=instance-state-name,Values=running" | jq '.Reservations[].Instances[].InstanceId')
+EC2_INSTANCE=$(aws ec2 describe-instances --region eu-west-2 --filters "Name=tag:Name,Values=dev-rds-bastion-host" "Name=instance-state-name,Values=running" | jq -r '.Reservations[].Instances[].InstanceId')
 
 aws ssm start-session \
     --region eu-west-2 \

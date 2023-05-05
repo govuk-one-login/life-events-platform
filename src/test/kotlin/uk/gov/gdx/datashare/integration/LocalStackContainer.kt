9ef455e71e25a8fe8d32328d22b80167c9827ca9
Ethan Mills <ethan.mills@softwire.com>
2023-05-05 11:14:32 +0100

    GPC-NONE: Bump localstack to 2.0.2

--- src/test/kotlin/uk/gov/gdx/datashare/integration/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/gdx/datashare/integration/LocalStackContainer.kt
@@ -24,7 +24,7 @@ object LocalStackContainer {
     if (localstackIsRunning()) return null
     val logConsumer = Slf4jLogConsumer(log).withPrefix("localstack")
     return LocalStackContainer(
-      DockerImageName.parse("localstack/localstack").withTag("1.3.1"),
+      DockerImageName.parse("localstack/localstack").withTag("2.0.2"),
     ).apply {
       withServices(LocalStackContainer.Service.SQS, LocalStackContainer.Service.SNS, LocalStackContainer.Service.S3)
       withEnv("DEFAULT_REGION", "eu-west-2")

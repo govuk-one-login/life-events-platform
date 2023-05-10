628d6bd32030865b12bdd2d9cd7300f607fa5b13
Oskar Williams <oskar.williams@softwire.com>
2023-05-10 11:20:11 +0100

    GPC-NONE: Add security contact phone number ssm param

---
+++ terraform/modules/policies/alternate_accounts.tf
@@ -0,0 +1,25 @@
+#resource "aws_account_alternate_contact" "security" {
+#
+#  alternate_contact_type = "SECURITY"
+#
+#  name          = "GDX Dev Team"
+#  title         = "Dev Team"
+#  email_address = "gdx-dev-team@digital.cabinet-office.gov.uk"
+#  phone_number  = sensitive(data.aws_ssm_parameter.security_contact_number.value)
+#}
+
+resource "aws_ssm_parameter" "security_contact_number" {
+  name  = "security-contact-number"
+  type  = "SecureString"
+  value = "+123456789"
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
+}
+
+data "aws_ssm_parameter" "security_contact_number" {
+  name = "DWPPrincipal"
+}

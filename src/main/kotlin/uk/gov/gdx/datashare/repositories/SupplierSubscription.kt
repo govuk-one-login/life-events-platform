11ba059f807e9b1f7bcd2b496f6a0e4a88c13a4c
oskarwilliams378 <oskarwilliams378@users.noreply.github.com>
2023-05-11 09:55:56 +0000

    GPC-NONE: Automatically lint files

--- src/main/kotlin/uk/gov/gdx/datashare/repositories/SupplierSubscription.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/repositories/SupplierSubscription.kt
@@ -36,7 +36,7 @@ data class SupplierSubscription(
   @JsonIgnore
   val new: Boolean = true,
 
-  ) : Persistable<UUID> {
+) : Persistable<UUID> {
   @JsonIgnore
   override fun getId(): UUID = id
 

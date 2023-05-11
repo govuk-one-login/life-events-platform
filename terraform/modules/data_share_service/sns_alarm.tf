26d85454d95ddb416b63006615a677f9bb9b1058
Ethan Mills <ethan.mills@softwire.com>
2023-05-11 16:49:52 +0100

    GPC-264: Fix cloudfront alarm

--- terraform/modules/data_share_service/sns_alarm.tf
+++ terraform/modules/data_share_service/sns_alarm.tf
@@ -8,3 +8,15 @@ module "sns" {
 
   arns_which_can_publish = [aws_prometheus_workspace.prometheus.arn]
 }
+
+module "sns-us-east-1" {
+  providers = {
+    aws = aws.us-east-1
+  }
+  source = "../sns"
+
+  account_id = data.aws_caller_identity.current.account_id
+  environment = var.environment
+  name = "gdx-alarms"
+  notification_emails = ["gdx-dev-team@digital.cabinet-office.gov.uk"]
+}

4253e44ab212230370fd45297eb57e2365033e28
CarGil <carly.gilson@softwire.com>
2023-05-11 17:09:25 +0100

    GPC-367: Replace gro death notification service with lambda calls

---
+++ src/main/kotlin/uk/gov/gdx/datashare/services/GroApiService.kt
@@ -0,0 +1,34 @@
+package uk.gov.gdx.datashare.services
+
+import net.minidev.json.JSONObject
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import uk.gov.gdx.datashare.config.NoDataFoundException
+import uk.gov.gdx.datashare.models.GroDeathRecord
+import uk.gov.gdx.datashare.models.GroEnrichEventResponse
+
+@Service
+class GroApiService(
+  private val lambdaService: LambdaService,
+  @Value("\${environment}") val environment: String,
+) {
+  fun enrichEvent(dataId: String): GroDeathRecord {
+    val jsonPayload = createEnrichEventPayload(dataId)
+
+    val res = lambdaService.invokeLambda("$environment-gro-ingestion-lambda-enrich-event", jsonPayload)
+    val parsedResponse = lambdaService.parseLambdaResponse(res, GroEnrichEventResponse::class.java)
+
+    if (parsedResponse.StatusCode.equals(HttpStatus.NOT_FOUND)) {
+      throw NoDataFoundException(dataId)
+    }
+
+    return parsedResponse.Event!!
+  }
+
+  private fun createEnrichEventPayload(dataId: String): String {
+    val jsonObj = JSONObject()
+    jsonObj["id"] = dataId
+    return jsonObj.toString()
+  }
+}
--- src/main/kotlin/uk/gov/gdx/datashare/services/GroDeathNotificationService.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/services/GroDeathNotificationService.kt
@@ -4,14 +4,13 @@ import com.amazonaws.xray.spring.aop.XRayEnabled
 import org.springframework.stereotype.Service
 import uk.gov.gdx.datashare.enums.EnrichmentField
 import uk.gov.gdx.datashare.enums.EventType
-import uk.gov.gdx.datashare.enums.GroSex
 import uk.gov.gdx.datashare.models.GroDeathNotificationDetails
-import uk.gov.gdx.datashare.models.GroDeathRecord
-import java.time.LocalDate
 
 @Service
 @XRayEnabled
-class GroDeathNotificationService : EnrichmentService {
+class GroDeathNotificationService(
+  private val groApiService: GroApiService,
+) : EnrichmentService {
   override fun accepts(eventType: EventType): Boolean {
     return eventType == EventType.GRO_DEATH_NOTIFICATION
   }
@@ -20,25 +19,8 @@ class GroDeathNotificationService : EnrichmentService {
     dataId: String,
     enrichmentFields: List<EnrichmentField>,
   ): GroDeathNotificationDetails {
-    val temporaryLevDeathRecord = GroDeathRecord(
-      hash = dataId,
-      registrationId = "registrationId",
-      eventTime = LocalDate.now(),
-      verificationLevel = "1",
-      dateOfDeath = LocalDate.now(),
-      partialMonthOfDeath = "2",
-      partialYearOfDeath = "2020",
-      forenames = "forenames",
-      surname = "surname",
-      maidenSurname = "maidenSurname",
-      sex = GroSex.FEMALE,
-      dateOfBirth = LocalDate.now(),
-      addressLine1 = "addressLine1",
-      addressLine2 = "addressLine2",
-      addressLine3 = "addressLine3",
-      addressLine4 = "addressLine4",
-      postcode = "postcode",
-    )
-    return GroDeathNotificationDetails.from(enrichmentFields, temporaryLevDeathRecord)
+    return groApiService
+      .enrichEvent(dataId)
+      .let { GroDeathNotificationDetails.from(enrichmentFields, it) }
   }
 }

8b4a24ccfd91f12fea2fa50f76f87ccf2235bc07
Oskar Williams <oskar.williams@softwire.com>
2023-05-11 08:40:27 +0100

    GPC-NONE: Add security account (#1126)
    * GPC-NONE: Add security account
    
    * GPC-NONE: Review comments
    
    * GPC-NONE: Retrigger workflows

--- terraform/modules/policies/alternate_accounts.tf
+++ terraform/modules/policies/alternate_accounts.tf
@@ -1,12 +1,11 @@
-#resource "aws_account_alternate_contact" "security" {
-#
-#  alternate_contact_type = "SECURITY"
-#
-#  name          = "GDX Dev Team"
-#  title         = "Dev Team"
-#  email_address = "gdx-dev-team@digital.cabinet-office.gov.uk"
-#  phone_number  = sensitive(data.aws_ssm_parameter.security_contact_number.value)
-#}
+resource "aws_account_alternate_contact" "security" {
+  alternate_contact_type = "SECURITY"
+
+  name          = "GDX Dev Team"
+  title         = "Dev Team"
+  email_address = "gdx-dev-team@digital.cabinet-office.gov.uk"
+  phone_number  = data.aws_ssm_parameter.security_contact_number.value
+}
 
 resource "aws_ssm_parameter" "security_contact_number" {
   name  = "security-contact-number"

3dd4a2ee47f6ace8675695beacb531540b254dd5
CarGil <carly.gilson@softwire.com>
2023-05-12 11:01:58 +0100

    GPC-367: Implement singleton lambda client

--- src/main/kotlin/uk/gov/gdx/datashare/services/LambdaService.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/services/LambdaService.kt
@@ -3,7 +3,6 @@ package uk.gov.gdx.datashare.services
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Service
 import software.amazon.awssdk.core.SdkBytes
-import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.lambda.LambdaClient
 import software.amazon.awssdk.services.lambda.model.InvokeRequest
 import software.amazon.awssdk.services.lambda.model.InvokeResponse
@@ -12,10 +11,10 @@ import software.amazon.awssdk.services.lambda.model.InvokeResponse
 class LambdaService(
   private val objectMapper: ObjectMapper,
 ) {
+  private val lambdaClient by lazy { LambdaClient.builder().build() }
+
   fun invokeLambda(functionName: String, jsonPayload: String): InvokeResponse {
     val payload = SdkBytes.fromUtf8String(jsonPayload)
-
-    val lambdaClient = createLambdaClient()
     val invokeRequest = createInvokeRequest(functionName, payload)
 
     return lambdaClient.invoke(invokeRequest)
@@ -25,11 +24,6 @@ class LambdaService(
     val value: String = response.payload().asUtf8String()
     return objectMapper.readValue(value, valueType)
   }
-
-  private fun createLambdaClient(): LambdaClient =
-    LambdaClient.builder()
-      .region(Region.EU_WEST_2)
-      .build()
 
   private fun createInvokeRequest(functionName: String, payload: SdkBytes): InvokeRequest =
     InvokeRequest.builder()

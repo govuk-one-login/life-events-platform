03b31e0d27be073570bf9727cdbd47dd617d6700
Oskar Williams <oskar.williams@softwire.com>
2023-05-17 11:10:27 +0100

    GPC-352: Fix tests

--- src/test/kotlin/uk/gov/gdx/datashare/services/ScheduledJobServiceTest.kt
+++ src/test/kotlin/uk/gov/gdx/datashare/services/ScheduledJobServiceTest.kt
@@ -4,6 +4,7 @@ import io.micrometer.core.instrument.MeterRegistry
 import io.mockk.*
 import net.javacrumbs.shedlock.core.LockAssert
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import uk.gov.gdx.datashare.helpers.TimeLimitedRepeater
 import uk.gov.gdx.datashare.repositories.AcquirerEventRepository
@@ -12,6 +13,7 @@ import uk.gov.gdx.datashare.repositories.SupplierEventRepository
 import uk.gov.gdx.datashare.services.GroApiService
 import uk.gov.gdx.datashare.services.ScheduledJobService
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
 class ScheduledJobServiceTest {
   private val acquirerEventRepository = mockk<AcquirerEventRepository>()
@@ -19,19 +21,25 @@ class ScheduledJobServiceTest {
   private val supplierEventRepository = mockk<SupplierEventRepository>()
   private val meterRegistry = mockk<MeterRegistry>()
 
-  private val underTest: ScheduledJobService = ScheduledJobService(
-    acquirerEventRepository,
-    groApiService,
-    supplierEventRepository,
-    meterRegistry,
-  )
+  private lateinit var underTest: ScheduledJobService;
+
+  @BeforeEach
+  fun init() {
+    every { acquirerEventRepository.countByDeletedAtIsNull() } returns 5
+    every { meterRegistry.gauge("UnconsumedEvents", any()) } returns AtomicInteger(5)
+    underTest = ScheduledJobService(
+      acquirerEventRepository,
+      groApiService,
+      supplierEventRepository,
+      meterRegistry,
+    )
+  }
 
   @Test
   fun `deleteConsumedGroSupplierEvents deletes multiple events`() {
     mockkStatic(LockAssert::class)
     every { LockAssert.assertLocked() } just runs
     mockkStatic(TimeLimitedRepeater::class)
-    every { TimeLimitedRepeater.repeat(any<List<SupplierEvent>>(), any()) } just runs
 
     val supplierEvent1 = SupplierEvent(
       supplierSubscriptionId = UUID.randomUUID(),
@@ -45,16 +53,12 @@ class ScheduledJobServiceTest {
     )
     val supplierEvents = listOf(supplierEvent1, supplierEvent2)
     every { supplierEventRepository.findGroDeathEventsForDeletion() } returns supplierEvents
+    every { TimeLimitedRepeater.repeat(supplierEvents, groApiService::deleteConsumedGroSupplierEvent) } just runs
 
     underTest.deleteConsumedGroSupplierEvents()
 
     verify(exactly = 1) {
-      TimeLimitedRepeater.repeat(
-        withArg<List<SupplierEvent>> {
-          assertThat(it).isEqualTo(supplierEvents)
-        },
-        any(),
-      )
+      TimeLimitedRepeater.repeat(supplierEvents, groApiService::deleteConsumedGroSupplierEvent)
     }
   }
 }

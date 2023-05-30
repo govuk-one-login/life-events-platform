44b05d89be2c0a60f7df39c84be3b02f54a7ce79
Oskar Williams <oskar.williams@softwire.com>
2023-05-30 11:43:28 +0100

    GPC-NONE: Fix delete acquirer subscription enrichment fields (#1232)

--- src/main/kotlin/uk/gov/gdx/datashare/repositories/AcquirerSubscriptionEnrichmentFieldRepository.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/repositories/AcquirerSubscriptionEnrichmentFieldRepository.kt
@@ -1,6 +1,8 @@
 package uk.gov.gdx.datashare.repositories
 
 import org.javers.spring.annotation.JaversSpringDataAuditable
+import org.springframework.data.jdbc.repository.query.Modifying
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.*
@@ -10,5 +12,7 @@ import java.util.*
 interface AcquirerSubscriptionEnrichmentFieldRepository : CrudRepository<AcquirerSubscriptionEnrichmentField, UUID> {
   fun findAllByAcquirerSubscriptionId(acquirerSubscriptionId: UUID): List<AcquirerSubscriptionEnrichmentField>
 
+  @Query("DELETE FROM acquirer_subscription_enrichment_field WHERE acquirer_subscription_id = :acquirerSubscriptionId")
+  @Modifying
   fun deleteAllByAcquirerSubscriptionId(acquirerSubscriptionId: UUID)
 }

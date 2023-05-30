92d84b59920e40864d93e5229764a5601af62598
CarlyG55 <carly.gilson@softwire.com>
2023-05-30 15:00:55 +0100

    GPC-296 move admin alerts to prod (#1238)
    * GPC-296: Remove admin alerts from shared
    
    * GPC-296: Add admin alerts to prod
    
    * GPC-296: Reorder regions
    
    * GPC-296: Readd eu-west-1 provider
    
    * GPC-296: Reorder providers

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -34,6 +34,26 @@ provider "aws" {
   }
 }
 
+provider "aws" {
+  alias  = "eu-west-1"
+  region = "eu-west-1"
+  default_tags {
+    tags = local.default_tags
+  }
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+  default_tags {
+    tags = local.default_tags
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
 data "terraform_remote_state" "dev" {
   backend = "s3"
 
@@ -89,4 +109,53 @@ module "route53" {
       name_servers = data.terraform_remote_state.shared.outputs.hosted_zone_name_servers
     }
   ]
+}
+
+module "sns" {
+  source = "../modules/sns"
+
+  account_id          = data.aws_caller_identity.current.account_id
+  environment         = local.env
+  region              = data.aws_region.current.name
+  name                = "sns"
+  notification_emails = ["gdx-dev-team@digital.cabinet-office.gov.uk"]
+}
+
+module "sns_eu_west_1" {
+  providers = {
+    aws = aws.eu-west-1
+  }
+  source = "../modules/sns"
+
+  account_id          = data.aws_caller_identity.current.account_id
+  environment         = local.env
+  region              = "eu-west-1"
+  name                = "sns"
+  notification_emails = ["gdx-dev-team@digital.cabinet-office.gov.uk"]
+}
+
+module "sns_us_east_1" {
+  providers = {
+    aws = aws.us-east-1
+  }
+  source = "../modules/sns"
+
+  account_id          = data.aws_caller_identity.current.account_id
+  environment         = local.env
+  region              = "us-east-1"
+  name                = "sns"
+  notification_emails = ["gdx-dev-team@digital.cabinet-office.gov.uk"]
+}
+
+module "assume_admin_role_alert" {
+  providers = {
+    aws           = aws
+    aws.eu-west-1 = aws.eu-west-1
+    aws.us-east-1 = aws.us-east-1
+  }
+  source = "../modules/assume_admin_role_alert_all_regions"
+
+  eu_west_2_sns_arn = module.sns.topic_arn
+  us_east_1_sns_arn = module.sns_us_east_1.topic_arn
+  eu_west_1_sns_arn = module.sns_eu_west_1.topic_arn
 }

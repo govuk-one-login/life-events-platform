d4ac207c40ead1b60e417839b3784c57ddd480c8
CarlyG55 <carly.gilson@softwire.com>
2023-05-30 10:25:47 +0100

    GPC-296: Add notifications for assuming admin role (#1229)
    * GPC-296: Add notifications for assuming admin role
    
    * GPC-NONE: Automatically lint files
    
    * GPC-296: Add providers for cloudwatch event targets
    
    * GPC-296: Add environment tp event rule names
    
    ---------
    
    Co-authored-by: CarlyG55 <CarlyG55@users.noreply.github.com>

--- terraform/modules/data_share_service/eventbridge.tf
+++ terraform/modules/data_share_service/eventbridge.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_event_rule" "tunnel_notification" {
-  name        = "bastion-tunnel-notification"
+  name        = "${var.environment}-bastion-tunnel-notification"
   description = "Send an SNS notification when a tunnel is initiated to the bastion host"
 
   event_pattern = jsonencode({
@@ -12,8 +12,98 @@ resource "aws_cloudwatch_event_rule" "tunnel_notification" {
   })
 }
 
-resource "aws_cloudwatch_event_target" "sns" {
+resource "aws_cloudwatch_event_target" "tunnel_notification" {
   rule      = aws_cloudwatch_event_rule.tunnel_notification.name
   target_id = "SendToSNS"
   arn       = module.sns.topic_arn
 }
+
+resource "aws_cloudwatch_event_rule" "admin_role_notification" {
+  name        = "${var.environment}-admin-role-notification"
+  description = "Send an SNS notification when a user assumes an admin role"
+
+  event_pattern = jsonencode({
+    "source" : ["aws.sts"],
+    "detail-type" : ["AWS API Call via CloudTrail"],
+    "detail" : {
+      "eventSource" : ["sts.amazonaws.com"],
+      "eventName" : ["AssumeRole"],
+      "requestParameters" : {
+        "roleArn" : [
+          {
+            "suffix" : "-admin"
+          }
+        ]
+      }
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_rule" "admin_role_notification_us_east_1" {
+  provider = aws.us-east-1
+
+  name        = "${var.environment}-admin-role-notification"
+  description = "Send an SNS notification when a user assumes an admin role"
+
+  event_pattern = jsonencode({
+    "source" : ["aws.sts"],
+    "detail-type" : ["AWS API Call via CloudTrail"],
+    "detail" : {
+      "eventSource" : ["sts.amazonaws.com"],
+      "eventName" : ["AssumeRole"],
+      "requestParameters" : {
+        "roleArn" : [
+          {
+            "suffix" : "-admin"
+          }
+        ]
+      }
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_rule" "admin_role_notification_eu_west_1" {
+  provider = aws.eu-west-1
+
+  name        = "${var.environment}-admin-role-notification"
+  description = "Send an SNS notification when a user assumes an admin role"
+
+  event_pattern = jsonencode({
+    "source" : ["aws.sts"],
+    "detail-type" : ["AWS API Call via CloudTrail"],
+    "detail" : {
+      "eventSource" : ["sts.amazonaws.com"],
+      "eventName" : ["AssumeRole"],
+      "requestParameters" : {
+        "roleArn" : [
+          {
+            "suffix" : "-admin"
+          }
+        ]
+      }
+    }
+  })
+}
+
+resource "aws_cloudwatch_event_target" "admin_role_notification" {
+  rule      = aws_cloudwatch_event_rule.admin_role_notification.name
+  target_id = "SendToSNS"
+  arn       = module.sns.topic_arn
+}
+
+resource "aws_cloudwatch_event_target" "admin_role_notification_us_east_1" {
+  provider = aws.us-east-1
+
+  rule      = aws_cloudwatch_event_rule.admin_role_notification_us_east_1.name
+  target_id = "SendToSNS"
+  arn       = module.sns-us-east-1.topic_arn
+}
+
+resource "aws_cloudwatch_event_target" "admin_role_notification_eu_west_1" {
+  provider = aws.eu-west-1
+
+  rule      = aws_cloudwatch_event_rule.admin_role_notification_eu_west_1.name
+  target_id = "SendToSNS"
+  arn       = module.sns-eu-west-1.topic_arn
+}
+
--- terraform/modules/data_share_service/sns_alarm.tf
+++ terraform/modules/data_share_service/sns_alarm.tf
@@ -18,7 +18,20 @@ module "sns-us-east-1" {
 
   account_id          = data.aws_caller_identity.current.account_id
   environment         = var.environment
-  region              = var.region
+  region              = "us-east-1"
+  name                = "gdx-alarms"
+  notification_emails = ["gdx-dev-team@digital.cabinet-office.gov.uk"]
+}
+
+module "sns-eu-west-1" {
+  providers = {
+    aws = aws.eu-west-1
+  }
+  source = "../sns"
+
+  account_id          = data.aws_caller_identity.current.account_id
+  environment         = var.environment
+  region              = "eu-west-1"
   name                = "gdx-alarms"
   notification_emails = ["gdx-dev-team@digital.cabinet-office.gov.uk"]
 }

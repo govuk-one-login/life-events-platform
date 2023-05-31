6ef26940307308e14584cc2def115daa981b7f32
Oskar Williams <oskar.williams@softwire.com>
2023-05-31 13:24:21 +0100

    GPC-NONE: Delete example consumer

--- terraform/modules/consumer/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {}
-variable "cloudwatch_retention_period" {}
-variable "region" {}
-variable "gdx_url" {}
-variable "auth_url" {}
-variable "consumer_client_id" {}
-variable "consumer_client_secret" {}
-variable "lev_api_url" {}
-variable "schedule" {}

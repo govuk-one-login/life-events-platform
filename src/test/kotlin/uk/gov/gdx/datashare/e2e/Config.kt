2871d6f522898175070e6be0b2c08f9694533df2
Oskar Williams <oskar.williams@softwire.com>
2023-06-05 09:52:12 +0100

    GPC-NONE: Tidy config private

--- src/test/kotlin/uk/gov/gdx/datashare/e2e/Config.kt
+++ src/test/kotlin/uk/gov/gdx/datashare/e2e/Config.kt
@@ -7,7 +7,7 @@ class Config {
     val environment = System.getenv("ENVIRONMENT") ?: "local"
     val apiUrl = System.getenv("API_URL") ?: "http://localhost:8080"
 
-    val cognitoTokenUrl = System.getenv("COGNITO_TOKEN_URL") ?: "http://localhost:9090/issuer1/token"
+    private val cognitoTokenUrl = System.getenv("COGNITO_TOKEN_URL") ?: "http://localhost:9090/issuer1/token"
     val cognitoTokenUri: URI = URI.create(cognitoTokenUrl)
     val adminClientId = System.getenv("COGNITO_CLIENT_ID") ?: "admin"
     val adminClientSecret = System.getenv("COGNITO_CLIENT_SECRET") ?: "admin"

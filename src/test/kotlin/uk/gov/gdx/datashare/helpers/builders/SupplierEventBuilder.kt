4db76011bcf6dc5d33ebf5ae22732f4e0414d6ff
CarGil <carly.gilson@softwire.com>
2023-06-05 11:06:38 +0100

    GPC-393: Add supplier builders

---
+++ src/test/kotlin/uk/gov/gdx/datashare/helpers/builders/SupplierEventBuilder.kt
@@ -0,0 +1,27 @@
+package uk.gov.gdx.datashare.uk.gov.gdx.datashare.helpers.builders
+
+import uk.gov.gdx.datashare.repositories.SupplierEvent
+import java.time.LocalDateTime
+import java.util.*
+
+data class SupplierEventBuilder(
+  var id: UUID = UUID.randomUUID(),
+  var supplierSubscriptionId: UUID = UUID.randomUUID(),
+  var dataId: String = "",
+  var eventTime: LocalDateTime? = null,
+  var createdAt: LocalDateTime = LocalDateTime.now(),
+  var deletedAt: LocalDateTime? = null,
+  var new: Boolean = false,
+) {
+  fun build(): SupplierEvent {
+    return SupplierEvent(
+      id = id,
+      supplierSubscriptionId = supplierSubscriptionId,
+      dataId = dataId,
+      eventTime = eventTime,
+      createdAt = createdAt,
+      deletedAt = deletedAt,
+      new = new,
+    )
+  }
+}

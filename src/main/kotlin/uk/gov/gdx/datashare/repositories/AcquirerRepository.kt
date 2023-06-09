14d8a6a53b76f7b11de8b09c1c99afd110ac8b80
Oskar Williams <oskar.williams@softwire.com>
2023-06-09 15:15:01 +0100

    GPC-NONE: Tag acquirer name (#1332)

--- src/main/kotlin/uk/gov/gdx/datashare/repositories/AcquirerRepository.kt
+++ src/main/kotlin/uk/gov/gdx/datashare/repositories/AcquirerRepository.kt
@@ -1,6 +1,7 @@
 package uk.gov.gdx.datashare.repositories
 
 import org.javers.spring.annotation.JaversSpringDataAuditable
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import java.util.*
@@ -9,4 +10,11 @@ import java.util.*
 @JaversSpringDataAuditable
 interface AcquirerRepository : CrudRepository<Acquirer, UUID> {
   fun findAllByWhenDeletedIsNull(): List<Acquirer>
+
+  @Query(
+    "SELECT a.name FROM acquirer a " +
+      "JOIN acquirer_subscription asub ON a.id = asub.acquirer_id " +
+      "WHERE asub.id = :id ",
+  )
+  fun findNameForAcquirerSubscriptionId(id: UUID): String
 }

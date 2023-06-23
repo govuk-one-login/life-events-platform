8c8cb52e07f7f66f49b5541a45c07952cf65f379
Oskar Williams <oskar.williams@softwire.com>
2023-06-23 13:55:13 +0100

    GPC-NONE: Add test lambda (#1458)
    * GPC-NONE: Add test lambda
    
    * GPC-NONE: Review comments
    
    * GPC-NONE: Revert gradle updates

---
+++ lambdas/test-lambda/src/test/java/uk/gov/di/data/lep/MaintTest.java
@@ -0,0 +1,22 @@
+package uk.gov.di.data.lep;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import uk.gov.di.data.lep.Main;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @Mock
+    private Context context;
+
+    private final Main underTest = new Main();
+
+    @Test
+    void handleRequestReturnsHello() {
+        var result = underTest.handleRequest("h", context);
+
+        assertEquals("hello", result);
+    }
+}

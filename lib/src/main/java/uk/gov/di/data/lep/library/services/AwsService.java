d8dfe74499e8749c34ff75b4f600a191ac7a4a1d
hanleo <hannah.leong@softwire.com>
2023-07-12 13:57:35 +0100

    Remove logging

--- lib/src/main/java/uk/gov/di/data/lep/library/services/AwsService.java
+++ lib/src/main/java/uk/gov/di/data/lep/library/services/AwsService.java
@@ -5,12 +5,9 @@ import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import uk.gov.di.data.lep.library.config.Config;
 
 public class AwsService {
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public static void putOnQueue(String message) {
         var sqsClient = AmazonSQSClientBuilder.standard()

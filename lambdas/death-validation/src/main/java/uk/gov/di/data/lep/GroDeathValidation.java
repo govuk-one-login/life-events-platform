386e25b88981f6d44f394d47b97c264de486b98c
hanleo <hannah.leong@softwire.com>
2023-07-12 16:09:28 +0100

    GPC-467: Fix validation lambda return

--- lambdas/death-validation/src/main/java/uk/gov/di/data/lep/GroDeathValidation.java
+++ lambdas/death-validation/src/main/java/uk/gov/di/data/lep/GroDeathValidation.java
@@ -3,6 +3,7 @@ package uk.gov.di.data.lep;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import uk.gov.di.data.lep.dto.GroDeathEvent;
@@ -11,11 +12,12 @@ import uk.gov.di.data.lep.library.dto.GroDeathEventBaseData;
 
 public class GroDeathValidation
     extends LambdaHandler<GroDeathEventBaseData>
-    implements RequestHandler<APIGatewayProxyRequestEvent, GroDeathEventBaseData> {
+    implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     @Override
-    public GroDeathEventBaseData handleRequest(APIGatewayProxyRequestEvent apiRequest, Context context) {
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiRequest, Context context) {
         var event = validateRequest(apiRequest);
-        return publish(event);
+        publish(event);
+        return new APIGatewayProxyResponseEvent().withStatusCode(201);
     }
 
     private GroDeathEventBaseData validateRequest(APIGatewayProxyRequestEvent event) {
--- lambdas/death-validation/src/test/java/uk/gov/di/data/lep/GroDeathValidationTest.java
+++ lambdas/death-validation/src/test/java/uk/gov/di/data/lep/GroDeathValidationTest.java
@@ -2,25 +2,56 @@ package uk.gov.di.data.lep;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import uk.gov.di.data.lep.library.dto.GroDeathEventBaseData;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.clearInvocations;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class GroDeathValidationTest {
     @Mock
     private Context context;
-    private final GroDeathValidation underTest = new GroDeathValidation();
+    private static GroDeathValidation underTest;
+
+    @BeforeAll
+    static void setup() {
+        underTest = mock(GroDeathValidation.class);
+        when(underTest.publish(any())).thenReturn(new GroDeathEventBaseData(("")));
+        when(underTest.handleRequest(any(), any())).thenCallRealMethod();
+    }
+
+    @BeforeEach
+    void refreshSetup(){
+        clearInvocations(underTest);
+    }
 
     @Test
-    void validateGroDeathEventDataReturnsBaseData() {
+    void validateGroDeathEventDataReturnsAPIGatewayProxyResponseEventWithStatusCode201() {
 
         var event = new APIGatewayProxyRequestEvent().withBody("{\"sourceId\":\"123a1234-a12b-12a1-a123-123456789012\"}");
 
         var result = underTest.handleRequest(event, context);
 
-        assertEquals("123a1234-a12b-12a1-a123-123456789012", result.sourceId());
+        assertEquals(201, result.getStatusCode());
+    }
+    @Test
+    void validateGroDeathEventDataPublishesBaseData() {
+
+        var event = new APIGatewayProxyRequestEvent().withBody("{\"sourceId\":\"123a1234-a12b-12a1-a123-123456789012\"}");
+
+        underTest.handleRequest(event, context);
+
+        verify(underTest).publish(argThat(x -> x.sourceId().equals("123a1234-a12b-12a1-a123-123456789012")));
     }
 
     @Test

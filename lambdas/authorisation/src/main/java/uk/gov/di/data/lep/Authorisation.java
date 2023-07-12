18678b42f21f0bdcb3d27a9c89e6c5f77f34cbdd
hanleo <hannah.leong@softwire.com>
2023-07-12 10:58:15 +0100

    GPC-NONE: Base set-up for lambda authorisation

---
+++ lambdas/authorisation/src/main/java/uk/gov/di/data/lep/Authorisation.java
@@ -0,0 +1,12 @@
+package uk.gov.di.data.lep;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Authorisation implements RequestHandler {
+
+    @Override
+    public Object handleRequest(Object input, Context context) {
+        return null;
+    }
+}

46076473394ba53c910e7985988c2e06fdde07e9
hanleo <hannah.leong@softwire.com>
2023-07-17 13:55:09 +0100

    GPC-466: Tidy

--- lib/src/main/java/uk/gov/di/data/lep/library/LambdaHandler.java
+++ lib/src/main/java/uk/gov/di/data/lep/library/LambdaHandler.java
@@ -2,13 +2,14 @@ package uk.gov.di.data.lep.library;
 
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import uk.gov.di.data.lep.library.config.Config;
 import uk.gov.di.data.lep.library.services.AwsService;
 
 public abstract class LambdaHandler<O> {
     public LambdaLogger logger;
+
     public O publish(O output) {
         String message;
         try {

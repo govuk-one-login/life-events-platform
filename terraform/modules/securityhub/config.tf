087268f540a3bb0e36adbaf5548d3a7f24e602f3
Oskar Williams <oskar.williams@digital.cabinet-office.gov.uk>
2023-07-25 16:23:12 +0100

    GPC-503: Remove securityhub

--- terraform/modules/securityhub/config.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_config_configuration_recorder" "config" {
-  name     = "config-recorder"
-  role_arn = var.config_role_arn
-  recording_group {
-    include_global_resource_types = true
-  }
-}
--- terraform/modules/securityhub/security_hub_exceptions.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_securityhub_standards_control" "controls" {
-  for_each = {
-    for index, rule in var.rules :
-    index => rule
-  }
-  standards_control_arn = "arn:aws:securityhub:${data.aws_region.current.name}:${var.account_id}:control/${each.value.rule}"
-  control_status        = "DISABLED"
-  disabled_reason       = each.value.disabled_reason
-}

812b29ab52152ba20fee53bc32591c46af2a1c02
Oskar Williams <oskar.williams@softwire.com>
2023-07-25 16:02:48 +0100

    GPC-503: Remove config delivery channel (#1726)

--- terraform/modules/securityhub/config.tf
+++ terraform/modules/securityhub/config.tf
@@ -5,16 +5,3 @@ resource "aws_config_configuration_recorder" "config" {
     include_global_resource_types = true
   }
 }
-
-resource "aws_config_configuration_recorder_status" "config" {
-  name       = aws_config_configuration_recorder.config.name
-  is_enabled = true
-  depends_on = [aws_config_delivery_channel.config]
-}
-
-resource "aws_config_delivery_channel" "config" {
-  name           = "config-delivery-channel"
-  s3_bucket_name = var.config_s3_id
-  s3_kms_key_arn = var.config_s3_kms_arn
-  depends_on     = [aws_config_configuration_recorder.config]
-}

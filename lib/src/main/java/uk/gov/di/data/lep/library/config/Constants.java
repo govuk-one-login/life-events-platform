4cb4b71ff390791a508d319f67708389e21167c2
Olly Levett <oliver.levett@digital.cabinet-office.gov.uk>
2023-08-03 10:55:22 +0100

    GPC-NONE: Tidy constant naming to match Java standards (#1827)

--- lib/src/main/java/uk/gov/di/data/lep/library/config/Constants.java
+++ lib/src/main/java/uk/gov/di/data/lep/library/config/Constants.java
@@ -1,6 +1,6 @@
 package uk.gov.di.data.lep.library.config;
 
 public class Constants {
-    public static final String localDatePattern = "yyyy-MM-dd";
-    public static final String localDateTimePattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS]";
+    public static final String LOCAL_DATE_PATTERN = "yyyy-MM-dd";
+    public static final String LOCAL_DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss[.SSS]";
 }

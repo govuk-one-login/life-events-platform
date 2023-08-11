00ad271cde24634b30e003e5d5013bec2a9dc61a
hanleo <hannah.leong@softwire.com>
2023-08-11 10:36:43 +0100

    GPC-534: Fix mappertest

--- lib/src/test/java/uk/gov/di/data/lep/library/services/MapperTest.java
+++ lib/src/test/java/uk/gov/di/data/lep/library/services/MapperTest.java
@@ -1,9 +1,6 @@
 package uk.gov.di.data.lep.library.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import uk.gov.di.data.lep.library.enums.GenderAtRegistration;
 import uk.gov.di.data.lep.library.enums.GroVerificationLevel;
@@ -14,17 +11,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class MapperTest {
-    private static ObjectMapper objectMapperUnderTest;
-    private static XmlMapper xmlMapperUnderTest;
-
-    @BeforeAll
-    static void setup() {
-        objectMapperUnderTest = new Mapper().objectMapper();
-        xmlMapperUnderTest = new Mapper().xmlMapper();
-    }
-
     @Test
     void ObjectMapperSerialisesStringToDateTimeFormat() throws JsonProcessingException {
+        var objectMapperUnderTest = new Mapper().objectMapper();
+
         var result = objectMapperUnderTest.readValue("{\"dateTime\":\"1958-06-06T12:30:57\"}", MapperTestObject.class);
 
         assertEquals(LocalDateTime.parse("1958-06-06T12:30:57"), result.dateTime);
@@ -32,6 +22,8 @@ class MapperTest {
 
     @Test
     void ObjectMapperSerialisesIntToGenderEnum() throws JsonProcessingException {
+        var objectMapperUnderTest = new Mapper().objectMapper();
+
         var maleTest = objectMapperUnderTest.readValue("{\"gender\":1}", MapperTestObject.class);
         var femaleTest = objectMapperUnderTest.readValue("{\"gender\":2}", MapperTestObject.class);
         var indeterminateTest = objectMapperUnderTest.readValue("{\"gender\":9}", MapperTestObject.class);
@@ -43,6 +35,8 @@ class MapperTest {
 
     @Test
     void ObjectMapperSerialisesStringToGroVerificationLevelEnum() throws JsonProcessingException {
+        var objectMapperUnderTest = new Mapper().objectMapper();
+
         var level0Test = objectMapperUnderTest.readValue("{\"verificationLevel\":\"00\"}", MapperTestObject.class);
         var level1Test = objectMapperUnderTest.readValue("{\"verificationLevel\":\"01\"}", MapperTestObject.class);
         var level2Test = objectMapperUnderTest.readValue("{\"verificationLevel\":\"02\"}", MapperTestObject.class);
@@ -56,6 +50,8 @@ class MapperTest {
 
     @Test
     void ObjectMapperReturnsNullForUnknownEnumValuesDuringSerialisation() throws JsonProcessingException {
+        var objectMapperUnderTest = new Mapper().objectMapper();
+
         var genderResult = objectMapperUnderTest.readValue("{\"gender\":\"\"}", MapperTestObject.class);
         var verificationLevelResult = objectMapperUnderTest.readValue("{\"verificationLevel\":\"\"}", MapperTestObject.class);
 
@@ -65,6 +61,7 @@ class MapperTest {
 
     @Test
     void XmlMapperSerialisesXmlToObject() throws JsonProcessingException {
+        var xmlMapperUnderTest = new Mapper().xmlMapper();
         var expected = new MapperTestObject();
         expected.dateTime = (LocalDateTime.parse("2023-06-03T12:34:56"));
         expected.gender = GenderAtRegistration.MALE;

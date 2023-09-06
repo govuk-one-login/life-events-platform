bf9ff6a13b7d2db89a33bf4b4271614246ec45fa
hannah-leong <hannah-leong@users.noreply.github.com>
2023-09-06 16:52:49 +0000

    GPC-NONE: Automatically lint files

--- lambdas/death-enrichment/src/test/java/uk/gov/di/data/lep/DeathEnrichmentTest.java
+++ lambdas/death-enrichment/src/test/java/uk/gov/di/data/lep/DeathEnrichmentTest.java
@@ -22,7 +22,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.mockStatic;

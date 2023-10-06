f32e97a38c1008d73ec0a42e791000a294c744ba
Ethan Mills <ethan.mills@digital.cabinet-office.gov.uk>
2023-10-06 09:10:33 +0100

    GPC-630: Replace encrypted config files with templates (#2362)
    To wind down the project, we wish to publish as much of the vpc config as we are able. It would be difficult to ensure a future team could decrypt these files.

--- stack-orchestration/encrypt_vpc_parameters.sh
+++
@@ -1,7 +0,0 @@
-PASSWORD="$(cat encryption_key.txt)"
-
-zip -er configuration/build/vpc.zip configuration/build/vpc -P "$PASSWORD"
-zip -er configuration/dev/vpc.zip configuration/dev/vpc -P "$PASSWORD"
-zip -er configuration/integration/vpc.zip configuration/integration/vpc -P "$PASSWORD"
-zip -er configuration/production/vpc.zip configuration/production/vpc -P "$PASSWORD"
-zip -er configuration/staging/vpc.zip configuration/staging/vpc -P "$PASSWORD"
--- stack-orchestration/provision_all.sh
+++ stack-orchestration/provision_all.sh
@@ -6,12 +6,20 @@ export AUTO_APPLY_CHANGESET=true
 export SKIP_AWS_AUTHENTICATION=true
 export AWS_PAGER=""
 
-./decrypt_vpc_parameters.sh
+## Check template values are substituted
+if [ -f configuration/"$AWS_ACCOUNT"/vpc/parameters.json ]; then
+  if ! awk '/<SFTP_IP>/ {offset = match($0, /<SFTP_IP>/); print "SFTP_IP parameter must be substituted at configuration/'"$AWS_ACCOUNT"'/vpc/parameters.json:" NR ":" offset; exit 1}' configuration/"$AWS_ACCOUNT"/vpc/parameters.json;
+  then
+    exit 1
+  fi
+else
+    echo "No VPC parameters found, skipping. See README for instructions on use of template files."
+fi
 
 ## Provision dependencies
 for dir in configuration/"$AWS_ACCOUNT"/*/; do
   STACK=$(basename "$dir")
-  if [[ $STACK != "sam-deploy-pipeline" ]]; then
+  if [[ $STACK != "sam-deploy-pipeline" && -f configuration/$AWS_ACCOUNT/$STACK/parameters.json ]]; then
     $PROVISION_COMMAND "$AWS_ACCOUNT" "$STACK" "$STACK" LATEST &
   fi
 done
